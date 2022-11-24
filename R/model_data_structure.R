a582e62b0d10770d06d988857b1d8093494e0ab8
stevencarlislewalker <stevencarlislewalker@gmail.com>
2022-11-24 14:40:54 -0500

    model structure spec implementation draft

--- R/model_data_structure.R
+++ R/model_data_structure.R
@@ -1,143 +1,197 @@
-# is_matrix_like = oor::ValidityMessager(
-#   test_function = oor::Any(
-#     oor::TestPipeline(
-#       oor::Summarizer(class),
-#       oor::Is("Mat")
-#     ),
-#     oor::All(
-#       is.numeric,
-#       oor::TestPipeline(
-#         oor::Summarizer(dim, length),
-#         oor::TestRange(0L, 2L)
-#       )
-#     )
+label_requirements = function(x) {
+  all(grepl("^[a-zA-Z]{1}[a-zA-Z0-9_]*$", x, perl = TRUE) | (x == ""), na.rm = FALSE)
+}
+
+validity_message = paste(
+  "Valid model variables have the following characteristics:",
+  "    They are data frames",
+  "    ",
+  sep = "\n"
+)
+
+# ValidityMessager(
+#   All(
+#     Is("data.frame"),
+#     MappedAllTest(is.character),
+#     TestPipeline(MappedSummarizer(names), MappedAllTest(is.null)),
+#
+#     ## bound the range of the number of variables
+#     ## (question: should we allow one variable or not?)
+#     TestPipeline(MappedSummarizer(length), MappedAllTest(Not(TestRange(0L, 0L)))),
+#     MappedAllTest(label_requirements),
+#
+#     ## bound the number of characters that is allowed in partitions
+#     TestPipeline(MappedSummarizer(nchar), MappedAllTest(Not(TestRange(0L, 0L)))),
+#
+#     TestPipeline(MappedSummarizer(length), TestHomo()),
+#     TestPipeline(Summarizer(names, is.null), TestFalse()),
+#     TestPipeline(Summarizer(names, duplicated, any), TestFalse()),
+#     TestPipeline(Summarizer(names), TestBasic(label_requirements))
 #   ),
-#   "Not a matrix-like object.",
-#   "Matrix-like objects need to ...",
-#   "  - be numeric vectors",
-#   "  - have no dim attribute or a dim attribute with two or fewer dimensions"
+#   "\nInvalid labelled partitions passed to ModelVars.",
+#   labelled_partitions_validity_message
 # )
 
-# is_matrix_like$assert("a")
-#
-# Mat = function(matrix_like) {
-#   self = Base()
-#   self$.matrix_like = is_matrix_like$assert(matrix_like)
-#   return_object(self, "Mat")
-# }
-#
-# Mats = function(...) {
-#   self = Base()
-#   self$
-# }
+#' Model Files
+#'
+#' Construct objects for accessing and caching model definition files.
+#'
+#' @param model_directory String giving a path to a directory containing
+#' the following files, `variables.csv`, `derivations.json`, `flows.csv`,
+#' and `settings.json`, described by
+#' [this spec](https://canmod.net/misc/model_definitions).
+#' @param csv_reader Class inheriting from \code{\link{Reader}} for reading
+#' csv files.
+#' @param json_reader Class inheriting from \code{\link{Reader}} for reading
+#' json files.
+#' @param txt_reader Class inheriting from \code{\link{Reader}} for reading
+#' txt files.
+#'
+#' @export
+ModelFiles = function(model_directory
+    , csv_reader = CSVReader
+    , json_reader = JSONReader
+    , txt_reader = TXTReader
+) {
+  self = Base()
+  self$.directory = normalizePath(model_directory)
+  self$.readers = list(
+    variables = csv_reader(self$.directory, "variables.csv"),
+    derivations = json_reader(self$.directory, "derivations.json"),
+    flows = csv_reader(self$.directory, "flows.csv"),
+    settings = json_reader(self$.directory, "settings.json")
+  )
+  self$.access_times = setNames(
+    rep(list(Sys.time()), length(self$.readers)),
+    names(self$.readers)
+  )
+  self$.components = setNames(
+    vector("list", length(self$.readers)),
+    names(self$.readers)
+  )
+  # read data and store it, bumping the access time
+  self$.fetch = function(component_name) {
+    self$.access_times[[component_name]] = Sys.time()
+    self$.components[[component_name]] =
+      self$.readers[[component_name]]$read()
+  }
+  # read data, store it, return it, bumping the access time
+  self$.read = function(component_name) {
+    self$.fetch(component_name)
+    self$.components[[component_name]]
+  }
+  # fill the components fields
+  self$.components = setNames(
+    lapply(names(self$.readers), self$.read),
+    names(self$.readers)
+  )
+  # fetch data only if it was last accessed before it changed
+  self$.pull = function(component_name) {
+    access_time = self$.access_times[[component_name]]
+    modification_time = file.mtime(self$.readers[[component_name]]$file)
+    if (modification_time > access_time) self$.fetch(component_name)
+  }
+  # pull data (i.e. fetch it only if necessary) and return it
+  self$.get = function(component_name) {
+    self$.pull(component_name)
+    self$.components[[component_name]]
+  }
+  self$variables = function() self$.get("variables")
+  self$derivations = function() self$.get("derivations")
+  self$flows = function() self$.get("flows")
+  self$settings = function() self$.get("settings")
 
-#
-#
-#
-#
-#
-#
-# TMBMacro = function(...) {
-#   self = Base()
-#   self$.input_list = list(...)
-#   self$to_tmb = function() {
-#     return(self$.input_list)
-#   }
-#   return_object(self, "TMBMacro")
-# }
-#
-#
-#
-# InputMatrices = function(...) {
-#   self = TMBMacro(...)
-#   return_object(self, "InputMatrices")
-# }
-#
-# Parameters = function(...) {
-#   self = TMBMacro(...)
-# }
-#
-#
-#
-#
-#
-#
-# valid_mats = function(mats) {
-#
-#   is_list =
-#   if (!is_list) return(FALSE)
-#
-#   is_empty = length(mats) == 0L
-#   if (is_empty) return(TRUE)
-#
-#   all_numeric =
-#   if (!all_numeric) return(FALSE)
-#
-#
-#   if (!all_mats) return(FALSE)
-#
-#   return(TRUE)
-# }
-#
-# valid_mats_config = function(mats_config) {
-#   is_list = is.list(mats_config)  # data frames etc are OK
-#   if (!is_list) return(FALSE)
-#
-#   good_names =
-#   if (!good_names) return(FALSE)
-#
-#   all_logical = all(vapply(mats_config, is.logical, logical(1L)))
-#   if (!all_logical) return(FALSE)
-#
-#   same_length = length(unique(vapply(mats_config, length, integer(1L)))) == 1L
-#   if (!same_length) return(FALSE)
-#
-#   return(TRUE)
-# }
-#
-# component_validity = list(
-#   mats = IsPipeline(
-#     IsStage(is_plain_list),
-#     AllInList(is.numeric),
-#     AllInList(IsDimRange(0L, 2L))
-#   ),
-#   mats_config = IsPipeline(
-#     IsStage(is.list),
-#     NamesEqual("mats_save_hist", "mats_return"),
-#     AllInList(is.logical),
-#     HomoList(length)
-#   ),
-#   params = IsPipeline(
-#     IsStage(is_plain_list),
-#
-#   )
-# )
-#
-#
-# component_validity$mats(initial_values$mats)
-# component_validity$mats_config(initial_values$mats_config)
-#
-# initial_values = list(
-#   mats = list(),
-#   mats_config = data.frame(
-#     mats_save_hist = logical(0L),
-#     mats_return = logical(0L)
-#   ),
-#   params = data.frame(
-#     params = numeric(0L),
-#     p_par_id = integer(0L),
-#     p_mat_id = integer(0L),
-#     p_row_id = integer(0L),
-#     p_col_id = integer(0L)
-#   ),
-#   random_index = data.frame(
-#     random = numeric(0L),
-#     r_par_id = integer(0L),
-#     r_mat_id = integer(0L),
-#     r_row_id = integer(0L),
-#     r_col_id = integer(0L)
-#   )
-# )
-#
-#
-#
+  return_object(self, "CompartmentalModel")
+}
+
+#' Reader
+#'
+#' Construct objects for reading data.
+#'
+#' @param ... Character vectors giving path components.
+#'
+#' @export
+Reader = function(...) {
+  self = Base()
+  self$file = normalizePath(file.path(...))
+  self$read = function() {
+    stop("Abstract class that is not implemented.")
+  }
+  return_object(self, "Reader")
+}
+
+#' @describeIn Reader Read CSV files.
+#' @export
+CSVReader = function(...) {
+  self = Reader(...)
+  self$read = function() {
+    read.table(
+      self$file, sep = ",", quote = "", na.strings = character(0L),
+      colClasses = "character", header = TRUE
+    )
+  }
+  return_object(self, "CSVReader")
+}
+
+#' @describeIn Reader Read JSON files.
+#' @importFrom jsonlite fromJSON
+#' @export
+JSONReader = function(...) {
+  self = Reader(...)
+  self$read = function() {
+    jsonlite::fromJSON(self$file, simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
+  }
+  return_object(self, "JSONReader")
+}
+
+#' @describeIn Reader Read TXT files.
+#' @export
+TXTReader = function(...) {
+  self = Reader(...)
+  self$read = function() readLines(self$file)
+  return_object(self, "TXTReader")
+}
+
+#' Model Starter
+#'
+#' Create a directory with a template model definition.
+#'
+#' @param starter_name Currently can only be \code{sir}.
+#' @param dir_name String giving the path to a directory for copying the
+#' template model definition.
+#'
+#' @export
+model_starter = function(starter_name, dir_name) {
+  starter_dir = system.file("starter_models", starter_name, package = "macpan2")
+  starter_files = list.files(starter_dir)
+  required_files = c(
+    variables_file = "variables.csv",
+    derivations_file = "derivations.json",
+    flows_file = "flows.csv",
+    settings_file = "settings.json"
+  )
+  if (!all(required_files %in% starter_files)) {
+    stop("Could not find a valid starter model by that name.")
+  }
+
+  starter_paths = setNames(
+    file.path(starter_dir, required_files),
+    names(required_files)
+  )
+
+  if (dir.exists(dir_name)) stop("Directory for the model already exists.")
+  dir.create(dir_name, recursive = TRUE)
+
+  file.copy(starter_paths, dir_name)
+  ModelFiles(dir_name)
+}
+
+#model_starter("seir", "../../../inst/starter_models/seir_symp")
+# sir_test_files = ModelFiles("starter_sir")
+# sir_test_files$variables()
+# sir_test_files$derivations()
+# sir_test_files$flows()
+# sir_test_files = model_starter("sir", "LDSKjf")
+#v = sir_test_files$derivations()
+#valid$is_variables_component$apply(v)
+#debug(valid$is_variables_component$apply)
