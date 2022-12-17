a002c1de6935097760ade428caed06c3230df0be
stevencarlislewalker <stevencarlislewalker@gmail.com>
2022-12-16 22:16:27 -0500

    segfault when assigning a matrix to itself

---
+++ misc/dev/dev-x-equals-x.R
@@ -0,0 +1,15 @@
+library(macpan2)
+library(TMB)
+
+compile('dev.cpp')
+dyn.load(dynlib("dev"))
+
+m = TMBModel(
+  MatsList(x = 0),
+  ExprList(before = list(x ~ x)),
+  OptParamsList(0),
+  OptParamsList(),
+  ObjectiveFunction(~0),
+  Time(0)
+)
+f = m$make_ad_fun("dev")
