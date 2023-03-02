964ce09af4f9ea2fd9adfcb71965a6d8e7caf594
Flynn-Primrose <DarrenFP@uoguelph.ca>
2023-03-02 14:14:03 -0500

    In progress work on derivations

--- R/model_data_structure.R
+++ R/model_data_structure.R
@@ -167,168 +167,125 @@ Model = function(definition) {
   self$variables = function() Partition(self$def$variables())
   self$flows = function() self$def$flows()
   self$flows_expanded = function() FlowExpander(self$def)$expand_flows()
-  self$rate_variables = function() {
+  self$flow_variables = function() {
     s = self$def$settings()
-    self$variables()$filter(s$rate_variables, .wrt = s$required_partitions)
+    self$variables()$filter(s$flow_variables, .wrt = s$required_partitions)
   }
   self$state_variables = function() {
     s = self$def$settings()
     self$variables()$filter(s$state_variables, .wrt = s$required_partitions)
   }
   self$derivations = self$def$derivations ## TODO: make this more useful
-  self$vctr_pointer = function(vrbl, vctr_nm = "state"){
-    vctr = self$def$settings()[[paste0(vctr_nm, "_variables")]]
-    return(as.numeric(which(vrbl == vctr))-1)# -1 because c++ side uses zero based indexing.
-  }
-  self$from_states = function(){
-    from_flows = self$flows_expanded()$from
-    return(lapply(from_flows, self$state_pointer))
-  }
-  self$to_states = function(){
-    to_flows = self$flows_expanded()$to
-    return(lapply(to_flows, self$state_pointer))
-  }
-  # self$init_vals = function(){
-  #   #TODO: obtain all required values.
-  # }
-  # self$init_mats = function(){
-  #   #TODO: initiate all required variables. Including:
-  #   #1) state, flow, rate
-  #   #2) from, to, from_lngth, to_lngth
-  #   #3) flow variables
-  # }
-  self$standard_expressions = function(){
-    flw_frml = MathExpressionFromStrings("state[from]*rate", list("state", "from", "rate"))
-    symblc_flw_frml = do.call(flw_frml$symbolic$evaluate, list("state", "from", "rate"))
-    
-    stt_frml = MathExpressionFromStrings("state - groupSums(state, from, from_lngth)+groupSums(state, to, to_lngth)", list("state", "from", "from_lngth", "to", "to_lngth"))
-    symblc_stt_frml = do.call(stt_frml$symbolic$evaluate, list("state", "from", "from_lngth", "to", "to_lngth"))
-    
-    return(list(list("flow", symblc_flw_frml), list("state", symblc_stt_frml)))
-  }
-  self$vctr_replacer = function(symbol, vrbls, vctr_nm = "state") {
-    if(any(symbol == vrbls)){
-      return(paste0(paste0(paste0(vctr_nm,"["), self$vctr_pointer(symbol, vctr_nm)), "]"))
-    }
-    else return(symbol)
-  }
-  self$user_expressions = function(){
-    stt_vrbls = self$state_variables()$labels()
-    rt_vrbls = self$rate_variables()$labels()
-    derivation_list = self$derivations()
-    nmbr_of_drvtns = length(derivation_list)
-    before = list()
-    during_pre_update = list()
-    during_post_update = list()
-    after = list()
-    for ( i in 1:nmbr_of_drvtns){
-      if(!is.null(derivation_list[[i]]$filter_partition)){
-        vrbls = self$variables()$filter(derivation_list[[i]]$filter_names, .wrt = derivation_list[[i]]$filter_partition)
-      } 
-      else vrbls = self$variables()
-      if(!is.null(derivation_list[[i]]$group_partition)){
-        nmbr_of_grps = length(derivation_list[[i]]$group_names)
-        grp_vrbls = lapply(derivation_list[[i]]$group_names, vrbls$filter, .wrt = derivation_list[[i]]$group_partition, .comparison_function = all_consistent)
-      }
-      else {
-        nmbr_of_grps = 1
-        grp_vrbls = list(vrbls)
-      }
-      if(!is.null(derivation_list[[i]]$output_partition)){
-        grp_outputs = lapply(derivation_list[[i]]$output_names, vrbls$filter, .wrt = derivation_list[[i]]$output_partition)
-      }
-      else {
-        grp_outputs = lapply(derivation_list[[i]]$output_names, vrbls$filter, .wrt = self$def$settings()$required_partitions)
-      }
-      if(!is.null(derivation_list[[i]]$input_partition)){
-        grp_inputs = derivation_list[[i]]$input_partition
-      }
-      else{
-        grp_inputs = self$def$settings()$required_partitions
-      }
-      
-      nondots_flag = !is.null(derivation_list[[i]]$arguments) #Does the derivation have regular (i.e. not related to dots) arguments
-      dots_flag = !is.null(derivation_list[[i]]$argument_dots) #Does the derivation have arguments to go in place of dots
-      
-      if(nondots_flag){
-        fltrd_grp_vrbls = list()
-        for(j in 1:nmbr_of_grps){
-          fltrd_grp_vrbls = c(fltrd_grp_vrbls, grp_vrbls[[j]]$filter(derivation_list[[i]]$arguments, .wrt = grp_inputs))
-        }
-      }
-      if(dots_flag){
-        #dots_flag = TRUE
-        fltrd_grp_vrbls_dts = list()
-        for(j in 1:nmbr_of_grps){
-          fltrd_grp_vrbls_dts = c(fltrd_grp_vrbls_dts, grp_vrbls[[j]]$filter(derivation_list[[i]]$argument_dots, .wrt = grp_inputs))
-        }
-      }
-      grp_exprs_list = list()
-      if(nondots_flag & dots_flag){
-        frml = MathExpressionFromStrings(derivation_list[[i]]$expression, derivation_list[[i]]$arguments, include_dots = TRUE)
-        for(j in 1:nmbr_of_grps){
-          ordrd_grp_vrbls = fltrd_grp_vrbls[[j]]$filter_ordered(derivation_list[[i]]$arguments, .wrt = grp_inputs)
-          symblc_input = c(ordrd_grp_vrbls$labels(), flted_grp_vrbls_dts[[j]]$labels())
-          symblc_input = lapply(symblc_input, self$vctr_replacer, stt_vrbls)
-          symblc_input = lapply(symblc_input, self$vctr_replacer, rt_vrbls, vctr_nm = "rate")
-          symblc_frml = do.call(frml$symbolic$evaluate, symblc_input)
-          symblc_output = grp_outputs[[j]]$labels()
-          symblc_output = lapply(symblc_output, self$vctr_replacer, stt_vrbls)
-          symblc_output = lapply(symblc_output, self$vctr_replacer, rt_vrbls, vctr_nm = "rate")
-          grp_exprs_list = append(grp_exprs_list, list(list(symblc_output, symblc_frml)))
-        }
-      }
-      else if(nondots_flag){
-        frml = MathExpressionFromStrings(derivation_list[[i]]$expression, derivation_list[[i]]$arguments)
-        for(j in 1:nmbr_of_grps){
-          ordrd_grp_vrbls = fltrd_grp_vrbls[[j]]$filter_ordered(derivation_list[[i]]$arguments, .wrt = grp_inputs)
-          symblc_input = ordrd_grp_vrbls$labels()
-          symblc_input = lapply(symblc_input, self$vctr_replacer, stt_vrbls)
-          symblc_input = lapply(symblc_input, self$vctr_replacer, rt_vrbls, vctr_nm = "rate")
-          symblc_frml = do.call(frml$symbolic$evaluate, symblc_input)
-          symblc_output = grp_outputs[[j]]$labels()
-          symblc_output = lapply(symblc_output, self$vctr_replacer, stt_vrbls)
-          symblc_output = lapply(symblc_output, self$vctr_replacer, rt_vrbls, vctr_nm = "rate")
-          grp_exprs_list = append(grp_exprs_list, list(list(symblc_output, symblc_frml)))
-        }
-      }
-      else if(dots_flag){
-        frml = MathExpressionFromStrings(derivation_list[[i]]$expression, include_dots = TRUE)
-        for(j in 1:nmbr_of_grps){
-          symblc_input = fltrd_grp_vrbls_dts[[j]]$labels()
-          symblc_input = lapply(symblc_input, self$vctr_replacer, stt_vrbls)
-          symblc_input = lapply(symblc_input, self$vctr_replacer, rt_vrbls, vctr_nm = "rate")
-          symblc_frml = do.call(frml$symbolic$evaluate, symblc_input)
-          symblc_output = grp_outputs[[j]]$labels()
-          symblc_output = lapply(symblc_output, self$vctr_replacer, stt_vrbls)
-          symblc_output = lapply(symblc_output, self$vctr_replacer, rt_vrbls, vctr_nm = "rate")
-          grp_exprs_list = append(grp_exprs_list, list(list(symblc_output, symblc_frml)))
-        }
-      }
-      else{
-        stop("Invalid derivations file? No arguments or argument dots.")
-      }
-      if(derivation_list[[i]]$simulation_phase == "before") before = append(before, list(grp_exprs_list))
-      else if(derivation_list[[i]]$simulation_phase == "during_pre_update") during_pre_update = append(during_pre_update, list(grp_exprs_list))
-      else if(derivation_list[[i]]$simulation_phase == "during_post_update") during_post_update = append(during_post_update, list(grp_exprs_list))
-      else if(derivation_list[[i]]$simulation_phase == "after") after = append(after, list(grp_exprs_list))
-      else stop("Unrecognized simulation phase")
-    }
-    usr_exprs = list(before, during_pre_update, during_post_update, after)
-    names(usr_exprs) = c("before", "during_pre_update", "during_post_update", "after")
-    return(usr_exprs)
-  }
-  # self$prep_expresions = function(){
-  #   #TODO: collect all expressions together into single list
-  #   #      Order is: 1. "before" expressions
-  #   #                2. "during_pre_update" expressions
-  #   #                3. assign variable rate expressions
-  #   #                4. standard expressions
-  #   #                5. "during_post_update" expressions
-  #   #                6. "after" expressions
-  #   # Note: 2 - 5 should be collected together as "during" expressions
-  # }
+
+  self$user_expressions = UserDefinedExpr(self$variables(), self$derivations(), self$def$settings(), self$flows())
+
   return_object(self, "Model")
+}
+
+
+
+
+DerivationExpander = function(model){
+  self = Base()
+  self$model = model
+  .filtered_variables = function(derivation){
+    if(!is.null(derivation$filter_partition)){
+      filtered_variables = variables$filter(derivation$filter_names, .wrt = derivation$filter_partition)
+    } 
+    else filtered_variables = variables
+    return(filtered_variables)
+  }
+  
+  .group_variables = function(derivation){
+    if(!is.null(derivation$group_partition)){
+      group_variables = lapply(derivation$group_names, filtered_variables$filter, .wrt = derivation$group_partition, .comparison_function = all_consistent)
+      # group_variables = lapply(derivation$group_names, self$.filtered_variables(derivation)$filter, .wrt = derivation$group_partition, .comparison_function = all_consistent)
+    }
+    else {
+      group_variables = list(filtered_variables)
+    }
+    return(group_variables)
+  }
+  
+  .group_outputs = function(filtered_variables, derivation, settings){
+    if(!is.null(derivation$output_partition)){
+      group_outputs = lapply(derivation$output_names, filtered_variables$filter, .wrt = derivation$output_partition)
+    }
+    else {
+      group_outputs = lapply(derivation$output_names, filtered_variables$filter, .wrt = settings$required_partitions)
+    }
+    return(group_outputs)
+  }
+  
+  .group_inputs = function(derivation, settings){
+    if(!is.null(derivation$input_partition)){
+      group_inputs = derivation$input_partition
+    }
+    else{
+      group_inputs = settings$required_partitions
+    }
+    return(group_inputs)
+  }
+  
+  .number_of_groups = function(derivation){
+    if(!is.null(derivation$output_partition)) number_of_groups = length(derivation$group_names)
+    else number_of_groups = 1
+    return(number_of_groups)
+  }
+  
+  .filtered_group_variables = function(derivation, group_variables, group_inputs){
+    filtered_group_variables = list()
+    if(!is.null(derivation$arguments)){
+      for(j in 1:self$.number_of_groups(derivation)){
+        filtered_group_variables = c(filtered_group_variables, group_variables[[j]]$filter(derivation$arguments, .wrt = group_inputs))
+      }
+    }
+    return(filtered_group_variables)
+  }
+   
+  .filtered_group_variable_dots = function(derivation, group_variables, group_inputs){ 
+    filtered_group_variable_dots = list()
+    if(!is.null(derivation$argument_dots)){
+      for(j in 1:self$.number_of_groups){
+        filtered_group_variable_dots = c(filtered_group_variable_dots, group_variables[[j]]$filter(derivation$argument_dots, .wrt = group_inputs))
+      }
+    }
+    return(filterd_group_variable_dots)
+  }
+  
+  expand_derivation = function(derivation){
+    
+  }
+  
+  expand_derivations = function(){
+    
+  }
+  
+  return_object(self, "DerivationExpander")
+}
+
+Scalar2Vector = function(){
+  self = Base()
+  #TODO: Replace scalar variables with the equivalent vector variables
+  return_object(self, "Scalar2Vector")
+}
+
+UserExpr = function(){
+  self = Base()
+  #TODO: Evaluate user provided expressions using variables obtained from Scalar2Vector.
+  return_object(self, "UserExpr")
+}
+
+StandardExpr = function(){
+  self = Base()
+  #TODO: Evaluate standard expressions
+  return_object(self, "StandardExpr")
+}
+
+ExpressionFormater = function(){
+  self = Base()
+  #TODO: Combine all user and standard expressions into a format that can be given to TMBModel.
+  return_object(self, "ExpressionFormater")
 }
 
 #' Reader
--- misc/dev/dev-model-def-to-tmb.R
+++ misc/dev/dev-model-def-to-tmb.R
@@ -8,30 +8,39 @@ model = function(model_name) {
 }
 model_paths = list.files(model_repo, full.names = TRUE)
 models = setNames(
-  lapply(model_paths, ModelFiles),
-  tools::file_path_sans_ext(basename(model_paths))
+  lapply(model_paths[3:7], ModelFiles),
+  tools::file_path_sans_ext(basename(model_paths[3:7]))
 )
 m = Model(models$seir_symp_vax)
 
-m$flow_variables()
-m$state_variables()
-m$variables()
-m$flows()
-m$flows_expanded()
-m$derivations()
+d = m$derivations()
+
+d1 = d[[1]]
+d4 = d[[4]]
 
 v = m$variables()
+ue = m$user_expressions()
+se = m$standard_expressions()
+m_from = m$from_states()
+m_to = m$to_states()
+fe = m$flows_expanded()
 
-v$labels()
-v$names()
-v$name()
-v$dotted()
+grp_list_d4 = group_expressions(v, d4)
+grp_list_d4
 
-v$select("Vax", "Epi")$labels()
-v$filter("I.unvax", "S.vax"
-  , .wrt = "Epi.Vax"
-  , .comparison_function = all_not_equal
-)
 
-## see macpan2:::make_expression for the current work on converting deriations
-## to expressions
+grp_list_d1 = group_expressions(v, d1)
+grp_list_d1
+
+grp_list_alt = group_expressions(v, d[[5]])
+grp_list_alt
+
+usr_exprs = user_expressions(v, d)
+usr_exprs
+
+m1 = Model(models$seir)
+ue1 = m1$user_expressions()
+m1_from = m1$from_states()
+m1_to = m$to_states()
+v1 = m1$variables()
+v1$filter()
