689dd73cda89986579ba3417fffb9965ed97846a
stevencarlislewalker <stevencarlislewalker@gmail.com>
2023-03-08 00:12:28 -0500

    simplify logic of engine_eval

--- R/engine_eval.R
+++ R/engine_eval.R
@@ -6,24 +6,24 @@
 #' This function is useful for trying out functions that can be used to
 #' define \code{macpan2} models.
 #'
-#' @param e Expression as a two-sided formula.
+#' @param e Expression as a one-sided formula, the right-hand-side of which is
+#' treated as the expression to be evaluated.
 #' @param ... Named objects that can be coerced into numeric matrices.
 #' @param .matrix_to_return Optional name of one of the matrices given
 #' in \code{...} to be returned. If this argument is missing, the
-#' matrix that will be returned is the matrix on the left-hand-side of the
-#' expression.
+#' matrix that will be returned is the matrix returned by the expression on
+#' the right-hand-side of the formula.
 #'
-#' @return Matrix being produced on the left-hand-side or matrix given in
+#' @return Matrix being produced on the right-hand-side or matrix given in
 #' \code{.matrix_to_return} if it is provided.
 #' @export
 #' @examples
-#' engine_eval(x ~ exp(y), y = pi) # ~ 23.14069
+#' engine_eval(~ exp(y), y = pi) # ~ 23.14069
 #'
 #' # It is not currently possible to assign values to a subset of
 #' # a matrix in a natural way (e.g. you cannot do things like x[1] = exp(y)),
 #' # but you can achieve this functionality using the assign function.
-#' engine_eval(dummy ~ assign(x, 1, 0, exp(y))
-#'   , dummy = empty_matrix
+#' engine_eval(~ assign(x, 1, 0, exp(y))
 #'   , x = rep(0, 2)
 #'   , y = pi
 #'   , .matrix_to_return = "x"
@@ -31,15 +31,25 @@
 engine_eval = function(e, ..., .matrix_to_return) {
   dot_mats = list(...)
 
-  if (missing(.matrix_to_return)) {
-    output_nm = as.character(e[[2L]])
-    output_mat = setNames(list(empty_matrix), output_nm)
-    init_mats = c(output_mat, dot_mats, .mats_to_return = output_nm)
+  ## force two-sided formula for compliance with TMBSimulator
+  ## that is used below
+  if (length(e) == 2L) {
+    left_hand_side = paste0(c("output", names(dot_mats)), collapse = "_")
+    e = as.formula(paste0(c(left_hand_side, as.character(e)), collapse = " "))
   } else {
-    output_nm = valid$char1$assert(.matrix_to_return)
-    output_mat = setNames(dot_mats[output_nm], output_nm)
-    init_mats = c(dot_mats, .mats_to_return = output_nm)
+    stop(
+      "\nThe expression must be given as the",
+      "\nright-hand-side of a one-sided formula."
+    )
   }
+
+  if (missing(.matrix_to_return)) .matrix_to_return = left_hand_side
+
+  init_mats = c(
+    setNames(list(empty_matrix), left_hand_side),
+    dot_mats,
+    .mats_to_return = .matrix_to_return
+  )
 
   m = TMBModel(
     init_mats = do.call(MatsList, init_mats),
@@ -50,6 +60,5 @@ engine_eval = function(e, ..., .matrix_to_return) {
     obj_fn = ObjectiveFunction(~0)
   )
 
-  TMBSimulator(m)$matrix(NA, matrix_name = output_nm, time_step = 1L)
+  TMBSimulator(m)$matrix(NA, matrix_name = .matrix_to_return, time_step = 1L)
 }
-
