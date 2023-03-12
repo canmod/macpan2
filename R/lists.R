1eaf3709b63606332628e3e3fec6f447c3bcbcc4
stevencarlislewalker <stevencarlislewalker@gmail.com>
2023-03-12 10:42:30 -0400

    depend on r version and unit testing

--- R/lists.R
+++ R/lists.R
@@ -16,36 +16,3 @@ nlist = function(...) {
     }
     setNames(L, nm)
 }
-
-#' Self-Named List
-#'
-#' Construct a list of strings with names given by those same strings.
-#'
-#' @param ... Character vectors
-#'
-#' @export
-self_named_list = function(...) {
-  x = as.character(c(...))
-  setNames(as.list(x), x)
-}
-
-#' Index a Named List of Character Vectors
-#'
-#' @param x Named list of character vectors
-#'
-#' @export
-ilist = function(x) {
-  #browser()
-  x = lapply(x, names)
-  nms = unlist(unname(x))
-  var_type_index = setNames(
-    unlist(mapply(rep, seq_along(x), each = unlist(lapply(unname(x), length)))),
-    nms
-  )
-  var_index = setNames(
-    unlist(lapply(unname(x), seq_along)),
-    nms
-  )
-  nlist(var_type_index, var_index)
-}
-
