4de31e92b2ff520350c8047dd0a6fe9a087cfb7d
stevencarlislewalker <stevencarlislewalker@gmail.com>
2023-03-15 13:29:00 -0400

    testing and roxygen

--- tests/testthat/test-sums.R
+++ tests/testthat/test-sums.R
@@ -3,4 +3,8 @@ test_that("summation works correctly", {
   A = matrix(1:12, 4, 3)
   expect_equal(engine_eval(~rowSums(A), A = A), cbind(c(15, 18, 21, 24)))
   expect_equal(engine_eval(~colSums(A), A = A), rbind(c(10, 26, 42)))
+  expect_equal(
+    engine_eval(~ groupSums(x, f, n), x = 1:10, f = rep(0:3, 1:4), n = 4),
+    matrix(c(1, 5, 15, 34))
+  )
 })
