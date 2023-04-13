3807336091141b218ac789e3b0e18ca8734dde61
stevencarlislewalker <stevencarlislewalker@gmail.com>
2023-04-13 11:36:15 -0400

    test assignment index checking

---
+++ tests/testthat/test-assign.R
@@ -0,0 +1,18 @@
+test_that("assignment indices are checked", {
+  x = rep(1, 5)
+  y = replace(x, c(2, 3), c(100, 200))
+  expect_equal(
+    engine_eval(~assign(x, c(1, 2), 0, c(100, 200))
+      , x = x
+      , .matrix_to_return = "x"
+    ),
+    matrix(y)
+  )
+  expect_error(
+    engine_eval(~assign(x, c(1, 5), 0, c(100, 200))
+      , x = x
+      , .matrix_to_return = "x"
+    ),
+    "Error thrown by the TMB engine"
+  )
+})
