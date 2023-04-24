5a2da0c3e84cccfd43d96b6cd41e25dda6acd06b
stevencarlislewalker <stevencarlislewalker@gmail.com>
2023-04-24 12:21:53 -0400

    [skip ci] product model prototypes

--- misc/experiments/products/product_funcs.R
+++ misc/experiments/products/product_funcs.R
@@ -10,17 +10,44 @@ variables_product = function(model1, model2){
 }
 
 flows_product = function(model1, model2){
+  # for model i:
+  #   - from_partition: from_partition_i
+  #   - to_partition: to_partition_i
+  #   - flow_partition: flow_partition_i
+  #   - from_to_partition: from_to_partition_i dot required_partition_j
+
+  dot = function(...) paste0(..., collapse = ".")
+  cdot = function(x, y) {
+    ifelse(
+      is_empty(x),
+      y,
+      paste(x, y, sep = ".")
+    )
+  }
+  is_empty = function(x) nchar(x) == 0L
+
   m1_flows = model1$flows()
-  m1_required_partitions = paste0(model1$def$settings()$required_partitions, collapse = ".")
-  
+  m1_required_partitions = dot(model1$def$settings()$required_partitions)
+  #m1_flows$from_to_partition = ifelse(
+  #  is_empty(m1_flows$from_to_partition),
+  #  m1_required_partitions,
+  #  m1_flows$from_to_partition
+  #)
+  print(m1_required_partitions)
+
   m2_flows = model2$flows()
-  m2_required_partitions = paste0(model2$def$settings()$required_partitions, collapse = ".")
-  
-  m1_flows$from_to_partition = m2_required_partitions
-  m1_flows$flow_partition = m2_required_partitions
-  
-  m2_flows$from_to_partition = m1_required_partitions
-  m2_flows$flow_partition = m1_required_partitions
-  
+  m2_required_partitions = dot(model2$def$settings()$required_partitions)
+  #m2_flows$from_to_partition = ifelse(
+  #  is_empty(m2_flows$from_to_partition),
+  #  m2_required_partitions,
+  #  m2_flows$from_to_partition
+  #)
+  print(m2_required_partitions)
+
+  m1_flows$from_to_partition = cdot(m1_flows$from_to_partition, m2_required_partitions)
+  m2_flows$from_to_partition = cdot(m2_flows$from_to_partition, m1_required_partitions)
+  m1_flows$from_flow_partition = cdot(m1_flows$from_flow_partition, m2_required_partitions)
+  m2_flows$from_flow_partition = cdot(m2_flows$from_flow_partition, m1_required_partitions)
+
   return(rbind(m1_flows, m2_flows))
 }
--- misc/experiments/products/products_testing.R
+++ misc/experiments/products/products_testing.R
@@ -2,23 +2,31 @@ library(oor)
 library(macpan2)
 library(TMB)
 
-model_path = "../../inst/starter_models/SI_products/"
-si_dir = paste0(model_path, "hello_si")
-age_dir = paste0(model_path, "hello_age")
-prod_dir = paste0(model_path, "hello_products")
+model_path = "inst/starter_models/SI_products"
+si_dir = file.path(model_path, "hello_si")
+age_dir = file.path(model_path, "hello_age")
+prod_dir = file.path(model_path, "hello_products")
+loc_dir = file.path(model_path, "hello_location")
 
 
 
 si_model = Compartmental(si_dir)
 age_model = Compartmental(age_dir)
 prod_model = Compartmental(prod_dir)
+loc_model = Compartmental(loc_dir)
+
 
 si_vars = si_model$variables$all()
 age_vars = age_model$variables$all()
+loc_vars = loc_model$variables$all()
+
 
 class(si_vars$names())
 
-variables_product(si_model, age_model)
+(si_model
+  |> variables_product(age_model)
+  |> variables_product(loc_model)
+)
 
 req_par_si = si_model$def$settings()$required_partitions
 req_par = prod_model$def$settings()$required_partitions
