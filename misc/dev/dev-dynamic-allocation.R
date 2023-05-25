abec91289e3847739dd57d756a9fd7b79afd4179
stevencarlislewalker <stevencarlislewalker@gmail.com>
2023-05-25 13:10:01 -0400

    [skip ci] slow euler example

--- misc/dev/dev-dynamic-allocation.R
+++ misc/dev/dev-dynamic-allocation.R
@@ -1,17 +1,19 @@
 library(macpan2)
-library(dplyr)
-
-macpan2:::dev_compile()
 
 ## choose c++ code to use:
 ##   1. "macpan2" to use the c++ in the installed package
 ##   2. "dev" to use the file for development in misc/dev/dev.cpp
-tmb_cpp = "dev"
+tmb_cpp = "macpan2"
 
+## if tmb_cpp = "dev", then you will need to compile it with this command
+macpan2:::dev_compile()
+
+## for profiling on SW's setup
 #dyn.load("/usr/local/lib/libprofiler.0.dylib")
-sir = Compartmental(file.path("inst", "starter_models", "sir"))
+
+sir = Compartmental(system.file("starter_models", "sir", package = "macpan2"))
 N = 100
-time_steps = 1000 ## Increase this number to cause allocation problems
+time_steps = 100000 ## Increase this number to cause allocation problems
 
 # --------
 simulator = sir$simulators$tmb(time_steps = time_steps
@@ -25,8 +27,5 @@ simulator = sir$simulators$tmb(time_steps = time_steps
 
 simulator$print$matrix_dims()
 simulator$print$expressions()
-
-## generate simulated data so that i can fit the generating
-## model to these data for a sanity check -- can we recover
-## the parameters from the simulating model?
-simulator$report(.phases = "during")
+sims = simulator$report(.phases = "during")
+head(sims)
