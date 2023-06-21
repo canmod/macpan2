6d7c2aff4dacb77ae6189c57e9e4067123e08751
Maya Earn <me.mayaearn@gmail.com>
2023-06-21 10:56:54 -0400

    update so that hosp occ calibration works with breakpoint hack

--- misc/experiments/calibration/calibration_ww.R
+++ misc/experiments/calibration/calibration_ww.R
@@ -1,4 +1,7 @@
-# currently, calibrating to hospital occupancy data is not working as expected.
+# currently, calibrating to hospital occupancy data is working, but it requires 
+# seemingly random breakpoints that don't work well for other data streams
+# and calibrating to multiple time series does not seem to be working properly 
+# with these breakpoints
 
 library(macpan2)
 library(dplyr)
@@ -11,7 +14,11 @@ breakpoint_dates = c(ymd(20200310), ymd(20200330), ymd(20200419), ymd(20200608),
                      ymd(20201115), ymd(20210124), ymd(20210213), ymd(20210305),
                      ymd(20210330), ymd(20210504), ymd(20210514))
 
-breakpoint_times = interval(ymd(20200310), breakpoint_dates) %/% days() + 1
+# add 120 days to breakpoint times and two random 
+# start breakpoint times, so that calibration works, 
+# does not make sense to me at all
+breakpoint_times = interval(ymd(20200310), breakpoint_dates) %/% days() + 1 + 120
+breakpoint_times = c(20, 60, breakpoint_times)
 
 # get macpan_base model with additional wastewater compartments
 macpan_ww = Compartmental(file.path("../../../", "inst", "starter_models", "ww"))
@@ -80,7 +87,7 @@ clean_data <- (champ_data
    %>% mutate(var = ifelse(var == "hosp.occ", "H", var))
    %>% mutate(date = interval(ymd("2020-04-08"), ymd(date)) %/% days())
    %>% distinct()
-   %>% mutate(time = date + 1)
+   %>% mutate(time = date + 1 + 60) #60 + 1)
 )
 
 # get observed waste vector and the corresponding time vector
@@ -104,7 +111,7 @@ obs_H_time_steps = (clean_data %>% filter(var == "H"))$time
 # observed_I = rpois(30, deterministic_prevalence)
 
 ## Step 0: set the number of time-steps required to fit the model
-simulator$replace$time_steps(460)
+simulator$replace$time_steps(460 + 60)
 
 ## Step 1: add observed data and declare matrices storing
 ##         the simulation history of variables to compare
@@ -125,20 +132,21 @@ simulator$add$matrices(
   , simulated_W = empty_matrix
   , simulated_H = empty_matrix
   , clamped_H = empty_matrix
+  , clamped_W = empty_matrix
   #, simulated_Adm = empty_matrix
   #, simulated_reports = empty_matrix
   # , simulated_report = empty_matrix
   , log_lik = empty_matrix
-  , .mats_to_save = c("simulated_W", "simulated_H", "total_inflow") # "simulated_report"
-  , .mats_to_return = c("log_lik", "simulated_W", "simulated_H", "total_inflow") # "simulated_report"
+  , .mats_to_save = c("simulated_W", "simulated_H", "total_inflow", "beta_changepoints") # "simulated_report"
+  , .mats_to_return = c("log_lik", "simulated_W", "simulated_H", "total_inflow", "beta_changepoints") # "simulated_report"
   #, .dimnames = list(total_inflow = list(macpan_ww$labels$state(), ""))
 )
 simulator$print$matrix_dims()
 
 # add time varying parameter tools
 simulator$add$matrices(
-  beta_changepoints = breakpoint_times # c(0,10,20,30,40,50,100,200,300) #breakpoint_times
-  , beta_values = c(0.2, 0.1, 0.2, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.1, 0.11, 0.12)
+  beta_changepoints = breakpoint_times # sort(c(5, 10, 20, 30, 40, breakpoint_times, 100, 110, 120, 130, 140)) #, 100, 120, 130, 140, 150, 190, 200, 210))# breakpoint_times)) # c(0,10,20,30,40,50,100,200,300) #breakpoint_times
+  , beta_values = 1:(length(breakpoint_times)) #*0.09 #c(0.1, 0.11, 0.2, 0.3, 0.4, 0.5, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.1, 0.11, 0.12, 0.13, 0.14, 0.15)
   , beta_pointer = 0
 )
 simulator$print$matrix_dims()
@@ -148,13 +156,13 @@ simulator$print$matrix_dims()
 ##         `.phase = "during"` indicates that this expression
 ##         should come at the end of the expressions evaluated
 ##         during each iteration of the simulation loop.
-# simulator$insert$expressions(
-#   #trajectory = simulated_I ~ 0.1 * sum(total_inflow[c(20, 21)])
-#   trajectory = simulated_W ~ W
-#   , .at = Inf  ## place the inserted expressions at the end of the expression list
-#   , .phase = "during"
-# )
-# simulator$print$expressions()
+simulator$insert$expressions(
+  #trajectory = simulated_I ~ 0.1 * sum(total_inflow[c(20, 21)])
+  trajectory = simulated_W ~ W
+  , .at = Inf  ## place the inserted expressions at the end of the expression list
+  , .phase = "during"
+)
+simulator$print$expressions()
 
 simulator$insert$expressions(
   trajectory = simulated_H ~ H
@@ -209,9 +217,11 @@ simulator$print$expressions()
 ##         iteration.
 simulator$insert$expressions(
     simulated_H ~ rbind_time(simulated_H, obs_H_time_steps)
+  , simulated_W ~ rbind_time(simulated_W, obs_W_time_steps)
     #simulated_H ~ log(1 + 1e-12 + exp(rbind_time(simulated_H, obs_H_time_steps)))
   #, simulated_H ~ exp(rbind_time(simulated_H, obs_H_time_steps))
   , clamped_H ~ 1e-12 + 0.5 * ((simulated_H - 1e-12) + ((simulated_H - 1e-12)^2 + (2*1e-6 - 1e-12)^2 - (1e-12)^2)^0.5)
+  , clamped_W ~ 1e-12 + 0.5 * ((simulated_W - 1e-12) + ((simulated_W - 1e-12)^2 + (2*1e-6 - 1e-12)^2 - (1e-12)^2)^0.5)
   , likelihood = log_lik ~
     (
       # dnorm(
@@ -223,6 +233,8 @@ simulator$insert$expressions(
       #   obs_W,
       #   clamp(rbind_time(simulated_W, obs_W_time_steps))
       # )
+      #+
+      #dpois(obs_W, clamped_W)
       #+
       dpois(obs_H, clamped_H)
       #+
@@ -261,8 +273,8 @@ simulator$print$expressions()
 # View(simulator$report(.phases = "after") %>% filter(matrix == "simulated_H"))
 
 ## Step 6: use the engine object
-#plot(obs_W_time_steps, obs_W)
-plot(obs_H_time_steps, obs_H)
+#plot(obs_W_time_steps, obs_W, xlim = c(1, 460 + 60))
+plot(obs_H_time_steps, obs_H, xlim = c(1, 460 + 60))
 
 simulator$optimize$nlminb()
 #simulator$optimize$optim()
@@ -270,9 +282,10 @@ simulator$current$params_frame()
 
 simulator$optimization_history$get()
 
-#lines(1:460, filter(simulator$report(.phases = "during"), matrix == "state", row == "W")$value, col = "red")
-lines(1:460, filter(simulator$report(.phases = "during"), matrix == "state", row == "H")$value, col = "red")
+#lines(1:(460+60), filter(simulator$report(.phases = "during"), matrix == "state", row == "W")$value, col = "red")
+lines(1:(460+60), filter(simulator$report(.phases = "during"), matrix == "state", row == "H")$value, col = "red")
 
+#520
 # simulator$cache$invalidate()
 # lines(1:460, filter(simulator$report(.phases = "during"), matrix == "state", row == "W")$value, col = "red")
 #simulator$optimization_history$get()
@@ -319,42 +332,42 @@ lines(1:460, filter(simulator$report(.phases = "during"), matrix == "state", row
 #   %>% filter(matrix == "simulated_W")
 #   %>% View
 # )
-library(macpan2)
-x = seq(from = -100, to = 100, by = 0.1)
-y = engine_eval(~ clamp(x, 10), x = x)
-plot(x, y, xlim = c(0, max(x)), ylim = c(0, max(y)), type = "l")
-abline(a = 0, b = 1, col = 2, lty = 2)
-
-cl = function(x, eps) {
-  # x + eps * (1.0 / (1.0 - (x - eps) / eps + ((x - eps) * (x - eps)) / (eps * eps)))
-  x + eps * (1.0 / (1.0 - (x - eps) / eps + ((x - eps) * (x - eps)) / (eps * eps)))
-}
-lines(x, cl(x, 10), col = 3)
-
-plot(x, plogis(x))
-
-cdf = function(x) plogis(x, mu, s)
-cdf = function(x) 1 / (1 + exp(-(x - mu)/s))
-cdf = function(x, mu, s) plogis(x, mu, s)
-cc = function(mu, s, x0) {
-  function(x) {
-    x0 * (1 - cdf(x, mu, s)) + x * cdf(x, mu, s)
-    #log(1 + exp(x))
-  }
-}
-plot(x, cc(1, 1, 1e-10)(x), type = 'l')
-plot(x, log(1 + 1e-12 + exp(x)), type = "l")
-sp = function(eps) function(x) (x + sqrt(x^2 + 4 * eps^2)) / 2
-plot(x, sp(1/10)(x), type = "l")
-sp(1e-6)(-1)
-abline(a = 0, b = 1)
-
-spp = function(eps_neg_inf, eps_0) {
-  function(x) {
-    eps_neg_inf + 0.5 * ((x - eps_neg_inf) + sqrt((x - eps_neg_inf)^2 + (2*eps_0 - eps_neg_inf)^2 - eps_neg_inf^2))
-  }
-}
-plot(x, spp(1e-12, 1e-6)(x), type = "l")
-spp(1e-12, 1e-6)(0.0001)
-spp(1e-12, 1e-6)(0)
-spp(1e-12, 1e-6)(-1e5)
+# library(macpan2)
+# x = seq(from = -100, to = 100, by = 0.1)
+# y = engine_eval(~ clamp(x, 10), x = x)
+# plot(x, y, xlim = c(0, max(x)), ylim = c(0, max(y)), type = "l")
+# abline(a = 0, b = 1, col = 2, lty = 2)
+# 
+# cl = function(x, eps) {
+#   # x + eps * (1.0 / (1.0 - (x - eps) / eps + ((x - eps) * (x - eps)) / (eps * eps)))
+#   x + eps * (1.0 / (1.0 - (x - eps) / eps + ((x - eps) * (x - eps)) / (eps * eps)))
+# }
+# lines(x, cl(x, 10), col = 3)
+# 
+# plot(x, plogis(x))
+# 
+# cdf = function(x) plogis(x, mu, s)
+# cdf = function(x) 1 / (1 + exp(-(x - mu)/s))
+# cdf = function(x, mu, s) plogis(x, mu, s)
+# cc = function(mu, s, x0) {
+#   function(x) {
+#     x0 * (1 - cdf(x, mu, s)) + x * cdf(x, mu, s)
+#     #log(1 + exp(x))
+#   }
+# }
+# plot(x, cc(1, 1, 1e-10)(x), type = 'l')
+# plot(x, log(1 + 1e-12 + exp(x)), type = "l")
+# sp = function(eps) function(x) (x + sqrt(x^2 + 4 * eps^2)) / 2
+# plot(x, sp(1/10)(x), type = "l")
+# sp(1e-6)(-1)
+# abline(a = 0, b = 1)
+# 
+# spp = function(eps_neg_inf, eps_0) {
+#   function(x) {
+#     eps_neg_inf + 0.5 * ((x - eps_neg_inf) + sqrt((x - eps_neg_inf)^2 + (2*eps_0 - eps_neg_inf)^2 - eps_neg_inf^2))
+#   }
+# }
+# plot(x, spp(1e-12, 1e-6)(x), type = "l")
+# spp(1e-12, 1e-6)(0.0001)
+# spp(1e-12, 1e-6)(0)
+# spp(1e-12, 1e-6)(-1e5)
