b3d0e7271034de3974947e3ce2f3695caa5c9039
stevencarlislewalker <stevencarlislewalker@gmail.com>
2023-11-06 04:47:53 -0500

    sir model structure file

--- inst/model_library/sir/model_structure.R
+++ inst/model_library/sir/model_structure.R
@@ -1,9 +1,25 @@
 library(macpan2)
 
+## dynamics ---------------------------------------------
+
+expr_list = mp_expr_list(
+  before = list(
+      N ~ sum(state)
+  ),
+  during = list(
+      flow_rates[infection_flow] ~ state[infectious_state] * trans_rates[transmission] / N
+    , flows_per_time ~ state[from] * flow_rates[edge]
+    , total_inflow ~ groupSums(flows_per_time, to, state)
+    , total_outflow ~ groupSums(flows_per_time, from, state)
+    , state ~ state + total_inflow - total_outflow
+  )
+)
+
 ## basic model indexes -------------------------
 
 state = mp_index(Epi = c("S", "I", "R"))
-rates = mp_index(Epi = c("lambda", "gamma", "beta"))
+flow_rates = mp_index(Epi = c("lambda", "gamma"))
+trans_rates = mp_index(Epi = "beta")
 
 ## model products (none in this model) --------
 
@@ -16,35 +32,28 @@ rates = mp_index(Epi = c("lambda", "gamma", "beta"))
 S      = mp_subset(state, Epi = "S")
 I      = mp_subset(state, Epi = "I")
 R      = mp_subset(state, Epi = "R")
-lambda = mp_subset(rates, Epi = "lambda")
-gamma  = mp_subset(rates, Epi = "gamma")
-beta   = mp_subset(rates, Epi = "beta")
+lambda = mp_subset(flow_rates, Epi = "lambda")
+gamma  = mp_subset(flow_rates, Epi = "gamma")
+beta   = mp_subset(trans_rates, Epi = "beta")
 
 ## links between entries in the indexes -----------
 
-mp_join(
-  infection = mp_join(from = S, to = I, flow = lambda),
-  recovery  = mp_join(from = I, to = R, flow = gamma)
+infection = mp_join(from = S, to = I, edge = lambda)
+recovery  = mp_join(from = I, to = R, edge = gamma)
+
+transmission =  mp_join(
+  infectious_state = I,
+  infection_flow = lambda,
+  transmission = beta
 )
 
-
-  influences = mp_index_data(
-    transmission = mp_join(
-      infectious = I,
-      infection = lambda,
-      transmission = beta
-    )
-  )
+index_data = list(
+    transmission = mp_index_data(transmission)  ## influences
+  , update = mp_index_data(infection, recovery)  ## flows
 )
 
-## expressions that define model dynamics ---------
-
-expr_list = mp_expr_list(
-  during = list(
-      rates[infection] ~ state[infectious] * rates[transmission] / N
-    , flows_per_time ~ state[from] * rates[flow]
-    , inflow ~ groupSums(flows_per_time, to, state)
-    , outflow ~ groupSums(flows_per_time, from, state)
-    , state ~ state + inflow - outflow
-  )
+vecs = list(
+  state = mp_vector(state),
+  flow_rates = mp_vector(flow_rates),
+  trans_rates = mp_vector(trans_rates)
 )
