1971a1ff8f4a6fc258ca77c191c93da2c60cefb6
stevencarlislewalker <stevencarlislewalker@gmail.com>
2024-01-09 08:49:58 -0500

    fix si tmb library model

--- inst/starter_models/si/tmb.R
+++ inst/starter_models/si/tmb.R
@@ -1,5 +1,5 @@
 library(macpan2)
-integrator = "hazard"
+integrator = "euler" ## could be euler, hazard, or rk4 (need to add demo_stoch)
 
 initialize_state = list(
     I ~ 1
@@ -7,41 +7,41 @@ initialize_state = list(
 )
 
 flow_rates = list(
-  euler = list(
+    euler = list(
       infection ~ beta * S * I / N
-  ),
-  ## expected value of the Euler-Binomial
-  hazard = list(
+    )
+  
+  , hazard = list( ## expected value of the Euler-Binomial
       infection ~ S * (1 - exp(-beta * I / N))
-  ),
+    )
+  , rk4 = list(
+        S_rk4 ~ S
+      , I_rk4 ~ I
+      , k1_infection ~ beta * S_rk4 * I_rk4 / N
+      
+      , S_rk4 ~ S_rk4 - k1_infection / 2
+      , I_rk4 ~ I_rk4 + k1_infection / 2
+      , k2_infection ~ beta * S_rk4 * I_rk4 / N
+      
+      , S_rk4 ~ S_rk4 - k2_infection / 2
+      , I_rk4 ~ I_rk4 + k2_infection / 2
+      , k3_infection ~ beta * S_rk4 * I_rk4 / N
+      
+      , S_rk4 ~ S_rk4 - k3_infection
+      , I_rk4 ~ I_rk4 + k3_infection
+      , k4_infection ~ beta * S_rk4 * I_rk4 / N
+      
+      , infection ~ (
+                k1_infection 
+          + 2 * k2_infection 
+          + 2 * k2_infection 
+          +     k4_infection
+        ) / 6
+    )
   ## (demographic stochasticity)
-  #flow_rates = list(
+  #, demo_stoch = flow_rates = list(
   #    infection ~ rbinom()  ## need to implement rbinom on the c++ side
   #)
-  rk4 = list(
-      S_rk4 ~ S
-    , I_rk4 ~ I
-    , k1_infection ~ beta * S_rk4 * I_rk4 / N
-    
-    , S_rk4 ~ S_rk4 - k1_infection / 2
-    , I_rk4 ~ I_rk4 + k1_infection / 2
-    , k2_infection ~ beta * S_rk4 * I_rk4 / N
-    
-    , S_rk4 ~ S_rk4 - k2_infection / 2
-    , I_rk4 ~ I_rk4 + k2_infection / 2
-    , k3_infection ~ beta * S_rk4 * I_rk4 / N
-    
-    , S_rk4 ~ S_rk4 - k3_infection
-    , I_rk4 ~ I_rk4 + k3_infection
-    , k4_infection ~ beta * S_rk4 * I_rk4 / N
-    
-    , infection ~ (
-              k1_infection 
-        + 2 * k2_infection 
-        + 2 * k2_infection 
-        +     k4_infection
-      ) / 6
-  )
 )
 
 update_state = list(
@@ -57,4 +57,4 @@ specs = lapply(flow_rates, \(flow_rates) {
   )
 })
 
-spec = specs[["euler"]]
+spec = specs[[integrator]]
