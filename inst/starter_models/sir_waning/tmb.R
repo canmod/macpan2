89bb86895734b9915f66a8adba2d5bd2e329ab46
stevencarlislewalker <stevencarlislewalker@gmail.com>
2024-01-09 08:39:51 -0500

    switch sir_waning tmb spec over to current standard

--- inst/starter_models/sir_waning/tmb.R
+++ inst/starter_models/sir_waning/tmb.R
@@ -17,35 +17,49 @@ state_updates = list(
   , R ~ R + recovery - waning_immunity
 )
 
-model_evaluation = list(
-  log_likelihood ~ dpois(I_obs, rbind_time(I, I_obs_times))
-)
+# model_evaluation = list(
+#   log_likelihood ~ dpois(I_obs, rbind_time(I, I_obs_times))
+# )
 
-expr_list =  ExprList(
+spec = mp_tmb_model_spec(
     before = computations
   , during = c(
       flow_rates
     , state_updates
   )
-  , after = model_evaluation
+  #, after = model_evaluation
+  , default = list(
+      S = 99, I = 1, R = 0
+    , beta = 0.2, gamma = 0.2, phi = 0.01
+  )
 )
 
-## set defaults
-init_mats = MatsList(
-    S = 99
-  , I = 1
-  , R = 0
-  , beta = 0.2
-  , gamma = 0.2
-  , phi = 0.01
-  , N = 100
-  , infection = empty_matrix
-  , recovery = empty_matrix
-  , waning_immunity = empty_matrix
-  , log_likelihood = empty_matrix
-  , I_obs = empty_matrix
-  , I_obs_times = empty_matrix
-  , .mats_to_save = c("I","R","waning_immunity")
-  , .mats_to_return = c("I","R","waning_immunity")
-)
 
+# expr_list =  ExprList(
+#     before = computations
+#   , during = c(
+#       flow_rates
+#     , state_updates
+#   )
+#   , after = model_evaluation
+# )
+# 
+# ## set defaults
+# init_mats = MatsList(
+#     S = 99
+#   , I = 1
+#   , R = 0
+#   , beta = 0.2
+#   , gamma = 0.2
+#   , phi = 0.01
+#   , N = 100
+#   , infection = empty_matrix
+#   , recovery = empty_matrix
+#   , waning_immunity = empty_matrix
+#   , log_likelihood = empty_matrix
+#   , I_obs = empty_matrix
+#   , I_obs_times = empty_matrix
+#   , .mats_to_save = c("I","R","waning_immunity")
+#   , .mats_to_return = c("I","R","waning_immunity")
+# )
+# 
