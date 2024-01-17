c362a1c4b616658f08e7ff28785b170d1cf01037
stevencarlislewalker <stevencarlislewalker@gmail.com>
2024-01-17 08:59:26 -0500

    behaviour of group_sums has changed

--- tests/testthat/test-sums.R
+++ tests/testthat/test-sums.R
@@ -4,7 +4,7 @@ test_that("summation works correctly", {
   expect_equal(engine_eval(~row_sums(A), A = A), cbind(c(15, 18, 21, 24)))
   expect_equal(engine_eval(~col_sums(A), A = A), rbind(c(10, 26, 42)))
   expect_equal(
-    engine_eval(~ group_sums(x, f, n), x = 1:10, f = rep(0:3, 1:4), n = 4),
+    engine_eval(~ group_sums(x, f, rep(0, n)), x = 1:10, f = rep(0:3, 1:4), n = 4),
     matrix(c(1, 5, 15, 34))
   )
 })
