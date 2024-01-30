c70ebb346864b345b3af7e7a8cf3a025c5c6e900
Jennifer Freeman <jenncfreeman@gmail.com>
2024-01-30 14:32:23 -0500

    able to simulate data for NFDS model (#136)

--- inst/starter_models/nfds/calibration_example.R
+++ inst/starter_models/nfds/calibration_example.R
@@ -16,18 +16,19 @@ spec
 
 # set number of time steps in simulation
 # believe time_steps = 120L is equal to 10 years (as in paper)
-time_steps = 2L 
+# monthly time steps
+time_steps = 120L 
 
 # simulator object
-# slow (~10s per time_step ...)
 start_time <- Sys.time()
 nfds = mp_simulator(  
-  model = spec
+    model = spec
   , time_steps = time_steps
-  , outputs = c("Y")
+  , outputs = c("Y","vax_strain")
 ) 
 end_time <- Sys.time()
 end_time-start_time
+
 
 
 ## -------------------------
@@ -36,8 +37,11 @@ end_time-start_time
 
 # we expect that prevalences of all genotypes should
 # reach equilibirium prevalences (level off) with sufficient time steps
+# although Colijn et al. (2020) mentions curves may not reach mathematical
+# equilibrium, but invasiveness equilibriates faster
 (nfds
   |> mp_trajectory()
+  |> filter(matrix=='Y')
   |> mutate(genotype=as.factor(row))
   |> ggplot()
   + geom_line(aes(time,value,col=genotype))
@@ -49,9 +53,17 @@ end_time-start_time
 # if genotype i is in the vaccine >0, else 0
 # we expect seroreplacement to take place, genotypes not in vaccine dominate population after
 # vaccine perturbation (with sufficient time steps)
+
+in_vax = (nfds
+          |> mp_trajectory()
+          |> filter(matrix=='vax_strain')
+          |> mutate(value=as.factor(value))
+)
+
 (nfds
   |> mp_trajectory()
-  |> mutate(in_vax = as.factor(rep(vax_strain,time_steps)))
+  |> filter(matrix=='Y')
+  |> mutate(in_vax = in_vax$value)
   |> group_by(time,in_vax)
   |> mutate(value = sum(value))
   |> ungroup()
@@ -64,6 +76,40 @@ end_time-start_time
 ## -------------------------
 ## specify objective function
 ## -------------------------
+
+# 1. infant IPD
+# evaluated after simulation loop once we have solution vector
+# read in invasiveness scores (OR)
+
+kids_invasiveness <- (read.csv(file.path("inst","starter_models","nfds","data","Invasiveness.kids.csv"), header = FALSE)
+                      # why doesn't this work
+                      # %>% as.matrix(dimnames = NULL)
+                      %>% as.matrix()
+)
+# dimnames seem to cause issues, so removing
+attr(kids_invasiveness, "dimnames") <- NULL 
+
+# negative log likelihood
+# obj_fn = ~ -sum(dpois(X_obs, rbind_time(X, X_obs_times)))
+
+obj_fn = ~ (1/N) * (Y %*% exp(kids_invasiveness))
+
+
+# update simulator to create new variables for invasiveness scores
+nfds$update$matrices(
+    kids_invasiveness = kids_invasiveness
+  , vax_strain = empty_matrix  
+)
+
+# update simulator to include this function
+nfds$replace$obj_fn(obj_fn)
+
+# how is next vax_strain chosen
+
+# 2. overall IPD
+
+# 3. AMR (anti-microbial resistance) IPD - includes both resistance and invasiveness
+
 
 
 ## -------------------------
--- inst/starter_models/nfds/tmb.R
+++ inst/starter_models/nfds/tmb.R
@@ -1,36 +1,57 @@
 library(macpan2)
 library(dplyr)
 
-computations = list(
-    ## total pop size
-    N ~ sum(Y)
-  
-    ## compute instantaneous locus frequencies (L total)
-  , f ~ (1/N) * (t(Y) %*% G)
-  
-    ## pi term in paper (M total), need to rename
-  , p ~ G %*% to_diag(w) %*% t(t(eq) - f) # is taking transpose slow computationally? (maybe a "flow")
-  
-)
-
-update_state = list(
-
-    Y ~ Y + (log(k/N) - r + rho*p)*Y + m ## why are brackets square for log[k/N], does this signify anything
-    
-    ## log(k/N) - enforces density dependent selection
-    ## r - characterizes the vaccine strategy
-    ## p - term that computes where prevalence is in relation to equilibrium frequencies (I think)
-)
-
+## -------------------------
+## local function 
 ## -------------------------
 
-## Get default values (this should maybe be included as a 'before' expression step, or defined in the simulation script)
-## read in input data (after data/write_data.m has been run)
+#' Binary Matrix Notation
+#'
+#' Get the indices of a binary matrix that have a value of 1,
+#' to simplify matrix multiplication.
+#' 
+#' Given a column vector \code{x, and binary Matrix \code{M}. We can 
+#' perform matrix multiplication by writing,
+#' \code{M %*% x}
+#' To simplify this computation, we can extract the indices of \code{M}
+#' that have a value of 1, reducing the dimension of the problem.
+#' These indices can then be used to identify entries in \code{x} that 
+#' should be grouped and summed over to compute \eqn{Mx}.
+#' 
+#'
+#' @param M matrix containing integers in \{0,1\}
+#' 
+#' @return named list of two integer vectors. 
+#' * \code{col_index} vector of column indices of M that have a value of 1,
+#' ordered by row. Indices start at 0, for C++ implementation.
+#' * \code{row_group} vector of row indices of M that have a value of 1,
+#' ordered by row. Indices start at 0, for C++ implementation.
+#' 
+#' This function was initially created to return integer vectors that can be
+#' used as inputs to `macpan2::group_sums(x[col_index],row_group,n)`
+#' 
+binary_matrix_notation <- function(M){
+  
+  col_index = c(t(col(M)*M))
+  col_index = as.integer(col_index[col_index!=0]-1) 
+  
+  row_group = as.integer(rep(1:nrow(M), times=rowSums(M))-1)
+  
+  return(nlist(col_index,row_group))
+}
+
+
+## -------------------------
+## read in model spec inputs
+## -------------------------
+
+## Get default parameter values and initial conditions used in (Colijn et al. 2020)
+## csv files were generated from Matlab script, nfds/data/write_data.m
 source_folder = file.path("inst","starter_models","nfds","data")
 
 ## names of source fields we need
 source_fields <- c(
-    "ics"               ## initial conditions
+   "ics"                ## initial conditions
   , "G"                 ## matrix encoding the genome data. Rows are isolates, cols are loci. G_{il} = 1 if strain i encodes locus l, 0 otherwise.
   , "SerotypeToStrain"  ## matrix encoding which serotype each strain is
   , "seronames"         ## names of the serotypes in the dataset, order is important. 
@@ -41,58 +62,129 @@ source_fields <- c(
   , "v"                 ## vaccine efficacy parameter
   , "m"                 ## migration (small parameter)
   , "Invasiveness.kids" ## invasiveness numbers from the meta-analysis, kids only
-  )
+)
 
 ## create one list object with named elements
 mass_data <- lapply(seq_along(source_fields), function(i){
   x <- (read.csv(file.path(source_folder,paste0(source_fields[i],".csv")), header=FALSE)
         %>% as.matrix() 
+        #%>% as.matrix(dimnames=NULL)# why doesn't this work 
   )
   attr(x,'dimnames')<- NULL
   x
 })
 names(mass_data) <- source_fields
 
-## create vector of vaccine strategy
+## -------------------------
+## vax formulation by serotype
+## -------------------------
 
-## total number of serotypes, (last one is always 'NT' for non-typables)
+# not totally sure where these should be defined,
+# I don't think they can go in defaults because 
+# they are not numeric, but might want them to 
+# be accessible from model spec
+included_serotypes = c('1','5','14')        ## serotypes names to be included in vaccine formulation, hardcoded in matlab code
+                                            ## believe these serotypes have to be in every vaccine formulation
+excluded_serotypes = c()                    ## serotypes names to be excluded in vaccine formulation (perhaps not needed)
+varying_serotypes = c('4', '6B', '9V', '14',
+                        '18C', '19F','23F') ## serotype names for vaccine PCV7, believe varying means optimization step
+                                            ## decides which to include/exclude (how this works TBD)
+
+## number of serotypes in data set is -1 because last one is always 'NT' for non-typables
 n_serotypes <- nrow(mass_data$seronames)-1
 
-## hardcoded as these serotypes should always be included
-included_serotypes <- c('1','5','14') 
-excluded_serotypes <- c()
-# varying_serotypes <- mass_data$seronames[1:n_serotypes,] %>% setdiff(union(included_serotypes, excluded_serotypes))
-## PCV7 serotypes 
-varying_serotypes <- c('4','6B', '9V', '14', '18C', '19F', '23F')
-
-
-## vax_serotypes[i] == 1, if serotype i is in the vaccine, 0 otherwise
+## attempted to use assign() to update vax_serotypes, but getting confusing error messages
+## updating locally for now
 vax_serotypes <- rep(0,n_serotypes)
 vax_serotypes[match(included_serotypes,mass_data$seronames[,1])] <- 1
 vax_serotypes[match(excluded_serotypes,mass_data$seronames[,1])] <- 0
 #setting to 1, think this is right, just need to tell optimizer that only these vars can be optimized 
 vax_serotypes[match(varying_serotypes,mass_data$seronames[,1])] <- 1 
 
-## now convert to level of genotype, is genotype i in the vaccine?
-v=0.0610 
-vax_strain <- as.numeric(mass_data$v)*(data.matrix(mass_data$SerotypeToStrain[,1:n_serotypes]) %*% matrix(vax_serotypes, nrow=length(vax_serotypes)))
+## -------------------------
+## get binary matrix indices
+## -------------------------
+
+Yind <- binary_matrix_notation(t(mass_data$G))
+
+pind <- binary_matrix_notation(mass_data$G)
+
+sero_mapping <- binary_matrix_notation(mass_data$SerotypeToStrain[,1:n_serotypes-1])
 
 ## -------------------------
+## expression lists
+## -------------------------
+
+vax_strategy = list(
+    ## map serotype to strain
+    vax_strain ~ group_sums(vax_serotypes[sero_to_strain], sero_group, vax_strain)
+
+    ## nfds vax term
+  , r ~ -log(1- (v*vax_strain))
+)
+
+computations = list(
+    ## total pop size
+    N ~ sum(Y)
+  
+    ## compute instantaneous locus frequencies from prevalences (L total)
+  , f ~ (1/N) * group_sums(Y[Y_i], Y_j, f)
+   
+    ## (L total) 
+  , weighted_deviation ~ w*(e - f)
+    
+    ## the 'NFDS' term, pi term in paper (M total)
+  , p ~ group_sums(weighted_deviation[p_i], p_j, p)
+  
+)
+
+update_state = list(
+
+    Y ~ Y + (log(k/N) - r + rho*p)*Y + m
+    
+    ## log(k/N) - enforces density dependent selection
+    ## r - characterizes the vaccine strategy
+)
 
 ## set defaults
 default = list(
     Y = mass_data$ics 
-  , G = mass_data$G
   , w = mass_data$locusweights
-  , eq = mass_data$locusfreq
-  , k = 1e5                      ## carrying capacity, hardcoded in matlab files
-  , r = -log(1-vax_strain)
-  , rho = log(1+mass_data$sigma) ## check (Corander et al. 2017) for possible description of sigma
-  , m = mass_data$m              ## migration, does this relate to \varepsilon or the "uniform migration rate" they talk about
+  , e = mass_data$locusfreq
+  , k = 1e5                                     ## carrying capacity, hardcoded in matlab files
+  , rho = log(1+mass_data$sigma)                ## check (Corander et al. 2017) for possible description of sigma
+  , m = mass_data$m                             ## migration, does this relate to \varepsilon or the "uniform migration rate" they talk about?
+  , f = rep(0,dim(mass_data$G)[2])              ## initialize f (instantaenouse locus frequencies) to zero vector of length L for group_sums 
+                                                ## L = number of loci = dim(mass_data$G)[2]
+  
+  , p = rep(0,dim(mass_data$G)[1])              ## initialize p (pi term in paper) to zero vector of length M for group_sums 
+                                                ## M = number of genotypes/strains = dim(mass_data$G)[1]
+  
+  , vax_serotypes = vax_serotypes               ## serotypes in vaccine
+  , vax_strain = rep(0,dim(mass_data$G)[1])     ## initialize vector of strains in vaccine, vax_strain[i]=1, if genotype i is in vaccine
+  , v = mass_data$v                             ## vaccine efficacy
+
+)
+
+integers = list(
+  
+    Y_i = Yind$col_index
+  , Y_j = Yind$row_group
+  , p_i = pind$col_index
+  , p_j = pind$row_group
+  
+  ## attempted to pass these indices to macpan2::assign to update vax_serotypes, but wasn't successful
+  # , in_vax = which(mass_data$seronames %in% c(included_serotypes, varying_serotypes))
+  # , not_in_vax = which(mass_data$seronames %in% excluded_serotypes)
+  
+  , sero_to_strain = sero_mapping$col_index
+  , sero_group = sero_mapping$row_group
 )
 
 ## model specification
 spec = mp_tmb_model_spec(
-    during = c(computations, update_state)
+    before = vax_strategy
+  , during = c(computations, update_state)
   , default = default
+  , integers = integers
 )
