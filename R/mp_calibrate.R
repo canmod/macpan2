71c92e1b06b2cdde56bb7f3d72cbb937ec915d92
stevencarlislewalker <stevencarlislewalker@gmail.com>
2024-02-01 16:02:09 -0500

    calibrate progress

---
+++ R/mp_calibrate.R
@@ -0,0 +1,11 @@
+#' @export
+mp_fixed_effects = function(model) UseMethod("mp_fixed_effects")
+
+#' @export
+mp_fixed_effects.TMBSimulator = function(model) {
+  fixed_effects = model$tmb_model$params$data_frame()
+  sd_report = sir_simulator$sdreport()
+  fixed_effects$estimate = sd_report$par_fixed
+  fixed_effects$std_error = sqrt(diag(sd_report$cov.fixed))
+  fixed_effects
+}
