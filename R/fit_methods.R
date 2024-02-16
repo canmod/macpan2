8901ffcc68ef6432d1fb8bdd76dd67201d4ccddf
stevencarlislewalker <stevencarlislewalker@gmail.com>
2024-02-16 14:19:18 -0500

    bad variable name

--- R/fit_methods.R
+++ R/fit_methods.R
@@ -4,7 +4,7 @@ mp_fixed_effects = function(model) UseMethod("mp_fixed_effects")
 #' @export
 mp_fixed_effects.TMBSimulator = function(model) {
   fixed_effects = model$tmb_model$params$data_frame()
-  sd_report = sir_simulator$sdreport()
+  sd_report = model$sdreport()
   fixed_effects$estimate = sd_report$par.fixed
   fixed_effects$std_error = sqrt(diag(sd_report$cov.fixed))
   fixed_effects
