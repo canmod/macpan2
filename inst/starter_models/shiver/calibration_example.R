236c51b3320de097d96f2e0cc5e9fea3bd812790
Jennifer Freeman <jenncfreeman@gmail.com>
2024-02-29 16:34:17 -0500

    fixing directory reference

--- inst/starter_models/shiver/calibration_example.R
+++ inst/starter_models/shiver/calibration_example.R
@@ -36,14 +36,13 @@ set.seed(expected_daily_reports)
 # Obtained from here:
 # https://data.ontario.ca/dataset/covid-19-vaccine-data-in-ontario/resource/274b819c-5d69-4539-a4db-f2950794138c
 daily_hospitalizations = (read.csv(
-  "inst/starter_models/shiver/data/hospitalizations_ontario.csv"
-  # system.file(
-  #   "starter_models"
-  #   , "shiver"
-  #   , "data"
-  #   , "hostpitalizations_ontario.csv"
-  #   , package = "macpan2"
-  # )
+  system.file(
+      "starter_models"
+    , "shiver"
+    , "data"
+    , "hostpitalizations_ontario.csv"
+    , package = "macpan2"
+  )
   , row.names = NULL
   ) 
   %>% rename(time = X_id)
