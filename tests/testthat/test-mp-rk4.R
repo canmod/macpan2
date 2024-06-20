93abd980f76b4f6a6e981dd264a9e8549b1912bc
Jennifer Freeman <jenncfreeman@gmail.com>
2024-06-20 08:14:13 -0400

    updated rk4 test with time_var

--- tests/testthat/test-mp-rk4.R
+++ tests/testthat/test-mp-rk4.R
@@ -17,21 +17,20 @@ test_that("mp_rk4() does not repeat preceding random computations to flows", {
 test_that("mp_rk4() does not interfere with preceding time step dependent functions", {
 
   change_pts = c(0,5,6)
-  beta_vals = c(0.2,0.8,0.5)
+  beta_diffs = c(0.2,0.8,-0.5)
+  beta_base = 1.1
   seir = mp_tmb_library("starter_models","seir",package = "macpan2")
   seir_beta = mp_tmb_insert(seir
                             , phase="during"
                             , at = 1L
-                            , expressions = list(beta ~ time_var(beta_vals,change_pts))
-                            , default = list(beta_vals = beta_vals)
-                            , integers = list(change_pts=change_pts))
+                            , expressions = list(beta ~ beta + time_var(beta_diffs,change_pts))
+                            , default = list(beta = beta_base, beta_diffs = beta_diffs)
+                            , integers = list(change_pts = change_pts))
   
-  # beta is always equal between two simulators
   euler_seir = seir_beta |> mp_euler() |> mp_simulator(10,c("beta")) |> mp_trajectory()
   rk4_seir = seir_beta |> mp_rk4() |> mp_simulator(10,c("beta")) |> mp_trajectory()
-  
+  # not currently equal (but should be)
   expect_equal(euler_seir, rk4_seir)
-  
 })
 
 test_that("mp_rk4() prevents modifications to state variables preceding flows", {
