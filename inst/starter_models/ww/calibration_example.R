4eb07973b4a32ced0b99a11efcad322b00fb4865
Jennifer Freeman <jenncfreeman@gmail.com>
2024-08-08 13:31:53 -0400

    updating expression lists and adding gamma convolution for wastewater model #156

--- inst/starter_models/ww/calibration_example.R
+++ inst/starter_models/ww/calibration_example.R
@@ -1,12 +1,197 @@
 library(macpan2)
 library(ggplot2)
 library(dplyr)
+options(macpan2_default_loss = "neg_bin") 
 
 ## -------------------------
-## get model spec from library
+## Model Specification
 ## -------------------------
 
-specs = mp_tmb_library("starter_models", "ww", alternative_specs = TRUE, package = "macpan2")
+source("./inst/starter_models/ww/tmb.R")
+#specs = mp_tmb_library("starter_models", "ww", alternative_specs = TRUE, package = "macpan2")
+
+# Update model specification to include additional components described here:
+# https://github.com/canmod/macpan2/issues/156
+focal_model = (specs$ww_euler
+               #specs$ww_hazard
+               
+               # add variable transformations:
+               |> mp_tmb_insert(phase = "before"
+                                , at = 1L
+                                , expressions = list(
+                                    zeta ~ exp(log_zeta)
+                                  , beta0 ~ exp(log_beta0)
+                                  , nonhosp_mort ~ 1/(1+exp((-logit_nonhosp_mort)))
+                                  , E ~ exp(log_E)
+                                  , Ca ~ exp(log_Ca)
+                                )
+                                , default = list(
+                                    log_zeta = empty_matrix
+                                  , log_beta0 = empty_matrix
+                                  , logit_nonhosp_mort = empty_matrix
+                                  , log_E = empty_matrix
+                                  , log_Ca = empty_matrix
+                                )
+                                
+               )
+               
+               # add accumulator variables:
+               # death - new deaths each time step
+               |> mp_tmb_insert(phase = "during"
+                                , at = Inf
+                                , expressions = list(death ~ ICUd.D + Is.D)
+               )
+               
+               # add phenomenological heterogeneity:
+               # might need to modify if we add time-varying transmission
+               |> mp_tmb_update(phase = "during"
+                                , at =1L
+                                , expressions = list(mp_per_capita_flow("S", "E", S.E ~ ((S/N)^zeta) * (beta0 / N) * (Ia * Ca + Ip * Cp + Im * Cm * (1 - iso_m) + Is * Cs *(1 - iso_s))))
+                                
+               )
+               
+               # compute gamma-density delay kernel for convolution:
+               |> mp_tmb_insert(phase = "before"
+                                , at = Inf
+                                , expressions = list(
+                                    gamma_shape ~ 1 / (c_delay_cv^2)
+                                  , gamma_scale ~ c_delay_mean * c_delay_cv^2
+                                  , gamma ~ pgamma(1:(qmax+1), gamma_shape, gamma_scale)
+                                  , delta ~ gamma[1:(qmax)] - gamma[0:(qmax - 1)]
+                                  , kappa ~ c_prop * delta / sum(delta)
+                                )
+                                , default = list(qmax = empty_matrix)
+               )
+               
+               # add convolution to compute case reports from incidence:
+               |> mp_tmb_insert(phase = "during"
+                                , at = Inf
+                                , expressions = list(report ~ convolution(S.E, kappa))
+               )
+)
+
+## -------------------------
+## Data Prep
+## -------------------------
+
+# For now, using simulated data to calibrate.
+
+# set number of time steps in simulation
+time_steps = 100L
+
+# width of report convolution kernel computed according to:
+# https://canmod.net/misc/flex_specs#computing-convolutions
+shape = 1/(mp_default_list(focal_model)$c_delay_cv^2)
+scale = mp_default_list(focal_model)$c_delay_mean * mp_default_list(focal_model)$c_delay_cv^2
+qmax = ceiling(qgamma(0.95, shape, scale))
+
+# simulated data
+sim_data = (focal_model 
+  |> mp_simulator(
+      time_steps = time_steps
+    , outputs = c("report","death", "W", "A")
+    , default = list(
+        S = 14.57e6 - 5 
+      , log_E = log(5)
+      , Ia = 0
+      , Ip = 0
+      , Im = 0
+      , Is = 0
+      , R = 0
+      , H = 0
+      , ICUs = 0
+      , ICUd = 0
+      , H2 = 0
+      , D = 0
+      , log_beta0=log(5)
+      , logit_nonhosp_mort = -0.5
+      , log_zeta = 1
+      , qmax = qmax
+      , log_Ca = log(2/3)
+      )
+  )
+  |> mp_trajectory()
+)
+
+## -------------------------
+## calibration
+## -------------------------
+
+focal_calib = mp_tmb_calibrator(
+    spec = focal_model
+  , data = sim_data
+  , traj = c("report","death", "W", "A") 
+  , par = c(
+      "log_Ca"
+    # negative binomial dispersion parameters for reports and deaths get added
+    # automatically with options(macpan2_default_loss = "neg_bin") set above
+  )
+  , default = list(
+    
+    # states
+    # Population of Ontario (2019) from:
+    # https://github.com/mac-theobio/macpan_base/blob/main/code/ontario_calibrate_comb.R
+      S = 14.57e6 - 5 
+    , log_E = log(5)
+    , Ia = 0
+    , Ip = 0
+    , Im = 0
+    , Is = 0
+    , R = 0
+    , H = 0
+    , ICUs = 0
+    , ICUd = 0
+    , H2 = 0
+    , D = 0
+    , W = 0
+    , A = 0
+    
+    # set initial parameter values for optimizer
+    , log_beta0=log(5)
+    , logit_nonhosp_mort = -0.5
+    , qmax = qmax
+    , log_zeta = 1
+    , log_Ca = log(1/3)
+  )
+)
+# converges
+mp_optimize(focal_calib)
+
+# Get fitted data
+fitted_data = mp_trajectory_sd(focal_calib, conf.int = TRUE)
+
+# parameter estimates
+mp_tmb_coef(focal_calib, conf.int = TRUE)
+
+# back transform Ca estimate
+# estimate is fairly off, perhaps trajectories are not informative enough
+# use Ia instead of reports?
+(mp_tmb_coef(focal_calib, conf.int = TRUE)[1,]
+  |> mutate(mat = "Ca")
+  |> mutate_if(is.numeric,exp)
+)
+
+if (interactive()) {
+
+  (ggplot(sim_data, aes(time,value))
+   + geom_point()
+   + geom_line(aes(time, value)
+               , data = fitted_data
+               , colour = "red"
+   )
+   + geom_ribbon(aes(time, ymin = conf.low, ymax = conf.high)
+                 , data = fitted_data
+                 , alpha = 0.2
+                 , colour = "red"
+   )
+   + facet_wrap(vars(matrix),scales = 'free')
+   + theme_bw()
+  )
+}
+
+## -------------------------
+## previous implementation
+## -------------------------
 
 # component = "during"
 # identical(
@@ -27,140 +212,19 @@ specs = mp_tmb_library("starter_models", "ww", alternative_specs = TRUE, package
 # char_expl[[34]]
 # char_impl[[34]]
 
-## -------------------------
-## define simulator
-## -------------------------
-
-# set number of time steps in simulation
-time_steps = 100L
-
-# simulator object
-ww = mp_simulator(  
-    model = specs$implicit
-  , time_steps = time_steps
-  , outputs = c("Ia", "Ip", "Im", "Is", "W", "A")
-)
-
-## -------------------------
-## parameterize model
-## -------------------------
-
-# interested in estimating asymptomatic relative transmission rate
-ww$update$transformations(Log("Ca"))
-ww$replace$params(log(specs$implicit$default$Ca),"log_Ca")
-ww
-
-## -------------------------
-## specify objective function
-## -------------------------
-
-
-# negative log likelihood
-obj_fn = ~ -sum(dpois(Ia_obs, rbind_time(Ia, Ia_obs_times)))
-
-# update simulator to create new variables 
-ww$update$matrices(
-    Ia_obs = empty_matrix
-  , Ia_obs_times = empty_matrix
-)
-
-# update simulator to include this function
-ww$replace$obj_fn(obj_fn)
-
-
-## -------------------------
-## simulate fake data
-## -------------------------
-
-# Ca value to simulate data with
-true_Ca = 0.8
-
-## simulate observed data using true parameters
-observed_data = ww$report(log(true_Ca))
-
-## compute incidence for observed data
-Ia_obs = rpois(time_steps, subset(observed_data, matrix == "Ia", select = c(value)) %>% pull())
-Ia_obs_times = subset(observed_data, matrix == "Ia", select = c(time)) %>% pull()
-
-if (interactive()) {
-  plot(Ia_obs, type = "l", las = 1)
-}
-
-
-## -------------------------
-## update simulator with fake data to fit to
-## -------------------------
-
-ww$update$matrices(
-    Ia_obs = Ia_obs
-  , Ia_obs_times = Ia_obs_times
-)
-
-## -------------------------
-## plot likelihood surface (curve)
-## -------------------------
-
-if (interactive()) {
-  
-  log_Ca_seq = seq(from = log(0.1), to = log(1), length = 100)
-  
-  ll = vapply(
-      log_Ca_seq
-    , ww$objective
-    , numeric(1L)
-  )
-  dat_for_plot <- (cbind(log_Ca_seq, ll)
-                   %>% data.frame()
-                   
-  )
-  
-  ggplot(dat_for_plot, aes(log_Ca_seq, ll)) +
-    geom_line()+
-    ## add true parameter values to compare
-    geom_vline(xintercept = log(true_Ca), col='red')+
-    xlab("log(Ca)")
-  
-}
-
-## -------------------------
-## fit parameters
-## -------------------------
-
-## optimize and check convergence
-ww$optimize$nlminb()
-
-## plot observed vs predicted
-if (interactive()) {
-  
-  ## estimate is close to true
-  print(ww$current$params_frame())
-  print(paste0("exp(default Ca) ",exp(ww$current$params_frame()$default)))
-  print(paste0("exp(current Ca) ",exp(ww$current$params_frame()$current)))
-  
-  data_to_plot <- (cbind(as.numeric(Ia_obs),1:time_steps)
-                   %>% data.frame()
-                   %>% setNames(c("value","time"))
-                   %>% mutate(type="observed")
-  ) %>% union(ww$report() 
-              %>% filter(matrix=="Ia") 
-              %>% select(time,value)
-              %>% mutate(type="predicted")
-  )
-  
-  ggplot(data_to_plot, aes(x=time, y=value, col=type, linetype = type))+
-    geom_line()+
-    theme_bw()+
-    ylab("Ia")
-  
-}
 
 ## -------------------------
 ## exploring
 ## -------------------------
+ww_exploring = mp_simulator(
+    model = specs$ww_euler
+  , time_steps = time_steps
+  , outputs = c("Ia", "Ip", "Im", "Is", "W", "A")
+) |> mp_trajectory()
 
 ## all infectious compartments
 if (interactive()) {
-  ggplot(ww$report() %>% filter(grepl("I",matrix))%>% select(time,value,matrix), aes(time,value,col=matrix))+
+  ggplot(ww_exploring %>% filter(grepl("I",matrix))%>% select(time,value,matrix), aes(time,value,col=matrix))+
     geom_line()+
     theme_bw()+
     ylab("individuals")
@@ -168,7 +232,7 @@ if (interactive()) {
 
 ## W
 if (interactive()) {
-  ggplot(ww$report() %>% filter(matrix=="W")%>% select(time,value), aes(time,value))+
+  ggplot(ww_exploring %>% filter(matrix=="W")%>% select(time,value), aes(time,value))+
     geom_line()+
     theme_bw()+
     ylab("W")
@@ -176,7 +240,7 @@ if (interactive()) {
 
 ## A
 if (interactive()) {
-  ggplot(ww$report() %>% filter(matrix=="A")%>% select(time,value), aes(time,value))+
+  ggplot(ww_exploring %>% filter(matrix=="A")%>% select(time,value), aes(time,value))+
     geom_line()+
     theme_bw()+
     ylab("A")
--- inst/starter_models/ww/tmb.R
+++ inst/starter_models/ww/tmb.R
@@ -4,47 +4,8 @@ computations = list(
   N ~ sum(S, E, Ia, Ip, Im, Is, R, H, ICUs, ICUd, H2, D)
 )
 
-flow_rates = list(
-    S.E ~ S * (beta0 / N) * (Ia * Ca + Ip * Cp + Im * Cm * (1 - iso_m) + Is * Cs *(1 - iso_s))
-  , E.Ia ~ E * alpha * sigma
-  , E.Ip ~ E * (1 - alpha)* sigma
-  , Ia.R ~ Ia * gamma_a
-  , Ip.Im ~ Ip * mu * gamma_p
-  , Im.R ~ Im * gamma_m
-  , Ip.Is ~ Ip * (1 - mu) * gamma_p
-  , Is.ICUs ~ Is * (1 - nonhosp_mort) * (1 - phi1) * (1 - phi2) * gamma_s
-  , Is.ICUd ~ Is * (1 - nonhosp_mort) * (1 - phi1) * phi2 * gamma_s
-  , ICUs.H2 ~ ICUs * psi1
-  , H2.R ~ H2 * psi3
-  , Is.H ~ Is * (1 - nonhosp_mort) * phi1 * gamma_s
-  , ICUd.D ~ ICUd * psi2
-  , H.R ~ H * rho
-  , Ia.W ~ Ia * nu
-  , Ip.W ~ Ip * nu 
-  , Im.W ~ Im * nu
-  , Is.W ~ Is * nu
-  , W.A ~ W * xi
-)
-
-state_updates = list(
-    S ~ S - S.E
-  , E ~ E + S.E - E.Ia - E.Ip
-  , Ia ~ Ia + E.Ia - Ia.R
-  , Ip ~ Ip + E.Ip - Ip.Im - Ip.Is
-  , Im ~ Im + Ip.Im - Im.R
-  , Is ~ Is + Ip.Is - Is.ICUs - Is.H - Is.ICUd
-  , H ~ H + Is.H - H.R
-  , ICUs ~ ICUs + Is.ICUs - ICUs.H2
-  , ICUd ~ ICUd + Is.ICUd - ICUd.D
-  , H2 ~ H2 + ICUs.H2 - H2.R
-  , R ~ R + Ia.R + Im.R + H.R + H2.R
-  , D ~ D + ICUd.D
-  , W ~ W + Ia.W + Ip.W + Im.W + Is.W - W.A
-  , A ~ A + W.A
-)
-
 foi = S.E ~ (beta0 / N) * (Ia * Ca + Ip * Cp + Im * Cm * (1 - iso_m) + Is * Cs *(1 - iso_s))
-flows = list(
+flow_rates = list(
     mp_per_capita_flow("S", "E", foi)
   , mp_per_capita_flow("E", "Ia", E.Ia ~ alpha * sigma)
   , mp_per_capita_flow("E", "Ip", E.Ip ~ (1 - alpha)* sigma)
@@ -53,10 +14,11 @@ flows = list(
   , mp_per_capita_flow("Im", "R", Im.R ~ gamma_m)
   , mp_per_capita_flow("Ip", "Is", Ip.Is ~ (1 - mu) * gamma_p)
   , mp_per_capita_flow("Is", "ICUs", Is.ICUs ~ (1 - nonhosp_mort) * (1 - phi1) * (1 - phi2) * gamma_s)
-  , mp_per_capita_flow("Is", "ICDd", Is.ICUd ~ (1 - nonhosp_mort) * (1 - phi1) * phi2 * gamma_s)
+  , mp_per_capita_flow("Is", "ICUd", Is.ICUd ~ (1 - nonhosp_mort) * (1 - phi1) * phi2 * gamma_s)
   , mp_per_capita_flow("ICUs", "H2", ICUs.H2 ~ psi1)
   , mp_per_capita_flow("H2", "R", H2.R ~ psi3)
   , mp_per_capita_flow("Is", "H", Is.H ~ (1 - nonhosp_mort) * phi1 * gamma_s)
+  , mp_per_capita_flow("Is", "D", Is.D ~ (nonhosp_mort) * gamma_s) 
   , mp_per_capita_flow("ICUd", "D", ICUd.D ~ psi2)
   , mp_per_capita_flow("H", "R", H.R ~ rho)
   , mp_per_capita_inflow("Ia", "W", Ia.W ~ nu)
@@ -65,23 +27,6 @@ flows = list(
   , mp_per_capita_inflow("Is", "W", Is.W ~ nu)
   , mp_per_capita_flow("W", "A", W.A ~ xi)
 )
-
-# state_updates = list(
-#     S ~ S - S.E
-#   , E ~ E + S.E - E.Ia - E.Ip
-#   , Ia ~ Ia + E.Ia - Ia.R
-#   , Ip ~ Ip + E.Ip - Ip.Im - Ip.Is
-#   , Im ~ Im + Ip.Im - Im.R
-#   , Is ~ Is + Ip.Is - Is.ICUs - Is.H - Is.ICUd
-#   , H ~ H + Is.H - H.R
-#   , ICUs ~ ICUs + Is.ICUs - ICUs.H2
-#   , ICUd ~ ICUd + Is.ICUd - ICUd.D
-#   , H2 ~ H2 + ICUs.H2 - H2.R
-#   , R ~ R + Ia.R + Im.R + H.R + H2.R
-#   , D ~ D + ICUd.D
-#   , W ~ W + Ia.W + Ip.W + Im.W + Is.W - W.A
-#   , A ~ A + W.A
-# )
 
 # set defaults
 default = list(
@@ -99,7 +44,6 @@ default = list(
   , rho          = 1/10     # 1/time in hospital (acute care)
   , delta        = 0        # Fraction of acute-care cases that are fatal
   , mu           = 0.956    # Fraction of symptomatic cases that are mild
-  #, E0           = 5        # Initial number exposed
   , nonhosp_mort = 0        # probability of mortality without hospitalization
   , iso_m        = 0        # Relative self-isolation/distancing of mild cases
   , iso_s        = 0        # Relative self-isolation/distancing of severe cases
@@ -113,8 +57,8 @@ default = list(
   , c_delay_cv   = 0.25     # coefficient of variation of testing delay
   , proc_disp    = 0        # dispersion parameter for process error (0=demog stoch only)
   , zeta         = 0        # phenomenological heterogeneity parameter
-  , nu           = 0.1        # something to do with waste-water
-  , xi           = 0.5        # something to do with waste-water
+  , nu           = 0.1      # something to do with waste-water
+  , xi           = 0.5      # something to do with waste-water
   # initial states
   , S = 1.00E+06
   , E = 1
@@ -133,16 +77,12 @@ default = list(
 )
 
 ## model spec
-specs =  list(
-    explicit = mp_tmb_model_spec(
-        before = computations
-      , during = flows
-      , default = default
-    )
-  , implicit = mp_tmb_model_spec(
-        before = computations
-      , during = c(flow_rates, state_updates)
-      , default = default
-    )
+spec = mp_tmb_model_spec(
+    before = computations
+  , during = flow_rates
+  , default = default
 )
-spec = specs[["implicit"]]
+specs = list(
+    ww_euler = spec
+  , ww_hazard = mp_hazard(spec)
+)
