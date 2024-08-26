0e5da307a9af9c88c9985d54a210b57dc9b2d9e5
Jennifer Freeman <jenncfreeman@gmail.com>
2024-08-26 16:00:44 -0400

    time-varying calibrator tests

---
+++ tests/testthat/test-calibrator-tv.R
@@ -0,0 +1,48 @@
+library(macpan2); library(testthat); library(dplyr); library(tidyr); library(ggplot2)
+test_that("rbf basis dimension is the same size as initial weights", {
+  spec = (
+    mp_tmb_library("starter_models", "sir", package = "macpan2") 
+      |> mp_tmb_insert("during", 1L
+         , expressions = list(beta ~ beta0 * beta1)
+         , default = list(beta0 = 0.5, beta1 = 1)
+    )
+  )
+  sim = mp_simulator(spec, 10, c("R","beta")) |> mp_trajectory()
+
+  expect_error(
+    mp_tmb_calibrator(spec
+      , data = sim
+      , traj = "R"
+      , par = "beta0"
+      , tv = mp_rbf("beta1",3, c())
+      , outputs = c("beta")
+    ),
+    regexp = "The `initial_weights` vector must be of length `dimension`"
+  )
+})
+# TO DO - determine why they are not identical
+# test_that("time-varying parameters values are consistent in calibrator object", {
+#   spec = (
+#     mp_tmb_library("starter_models", "sir", package = "macpan2")
+##       |> mp_tmb_insert("during", 1L
+#          , expressions = list(beta ~ beta0 * beta1)
+#          , default = list(beta0 = 0.5, beta1 = 1)
+#     )
+#   )
+#   sim = mp_simulator(spec, 10, c("R","beta")) |> mp_trajectory()
+#   
+#   set.seed(10)
+#   beta1_val = runif(10)
+#   beta1_df = data.frame(matrix = "beta1", time = seq(1:10), value = beta1_val)
+#   
+#   expect_identical(
+#     mp_tmb_calibrator(spec
+#                       , data = bind_rows(sim, beta1_df)
+#                       , traj = "R"
+#                       , par = "beta0"
+#                       , tv = "beta1"
+#                       , outputs = c("beta1")
+#     ) |> mp_trajectory() |> select(value),
+#     beta1_val
+#   )
+# })
