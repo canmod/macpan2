f9632974a154aa08e1e83479f9f30dd2f99c0c85
stevencarlislewalker <stevencarlislewalker@gmail.com>
2024-12-30 21:20:48 -0500

    testing

--- tests/testthat/test-repeat-flows.R
+++ tests/testthat/test-repeat-flows.R
@@ -1,19 +1,12 @@
 test_that("repeated flow names get flagged", {
-  spec = mp_tmb_model_spec(
-      during = list(
-          mp_per_capita_flow("S", "I", "beta * I", "repeated_flow_name")
-        , mp_per_capita_flow("I", "R", "gamma"   , "repeated_flow_name")
+  expect_error(
+      mp_tmb_model_spec(
+          during = list(
+              mp_per_capita_flow("S", "I", "beta * I", "repeated_flow_name")
+            , mp_per_capita_flow("I", "R", "gamma"   , "repeated_flow_name")
+          )
+        , default = list(S = 1 - 1e-5, I = 1e-5, beta = 0.25, gamma = 0.1)
       )
-    , default = list(S = 1 - 1e-5, I = 1e-5, beta = 0.25, gamma = 0.1)
-  )
-  spec$change_model$flow_frame()
-  spec$change_model$duplicated_change_names()
-  expect_error(
-      spec$change_model$check()
-    , "The following names are duplicates"
-  )
-  expect_error(
-      mp_rk4(spec)$change_model$check()
     , "The following names are duplicates"
   )
 })
