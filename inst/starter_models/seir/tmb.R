15f0c9d325bf3359e81ca8347a428b08428c4d0f
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-03-02 07:59:30 -0500

    fix little mistake

--- inst/starter_models/seir/tmb.R
+++ inst/starter_models/seir/tmb.R
@@ -7,7 +7,6 @@ initialize_state = list(
 flows = list(
     mp_per_capita_flow("S", "E", "beta * I / N", "exposure")
   , mp_per_capita_flow("E", "I", "alpha", "infection")
-  , S ~ N - I - R - E
   , mp_per_capita_flow("I", "R", "gamma", "recovery")
 )
 
