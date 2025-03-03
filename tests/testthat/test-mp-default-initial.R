f185120b652585b7edb3a4c71e73d83365d3a221
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-03-03 13:16:42 -0500

    better test

--- tests/testthat/test-mp-default-initial.R
+++ tests/testthat/test-mp-default-initial.R
@@ -1,4 +1,4 @@
-library(macpan2); library(testthat); library(dplyr); library(tidyr); library(ggplot2)
+options(macpan2_verbose = FALSE)
 spec = mp_tmb_library("starter_models", "sir", package = "macpan2")
 test_that("default/initial values in specs/sims are consistent", {
   updates = list(N = 10000, beta = 0.3)
@@ -26,7 +26,8 @@ test_that("default/initial values in specs/sims are consistent", {
     , initial_sim[order(initial_sim$matrix), , drop = FALSE]
     , ignore_attr = TRUE
   )
-  expect_equal(initial_list_spec, initial_list_sim)
+  expect_setequal(names(initial_list_spec), names(initial_list_sim))
+  expect_equal(initial_list_spec[names(initial_list_sim)], initial_list_sim)
 })
 
 test_that("optimized calibration impacts initial values properly", {
@@ -45,29 +46,26 @@ test_that("optimized calibration impacts initial values properly", {
     , par = c("beta", "gamma")
     , default = list(beta = 0.25, gamma = 0.17)
   )
-  mp_default(cal)
-  mp_initial(cal)
+  
+  expect_equal(mp_initial_list(cal)$R0[,], 0.25/0.17)
   mp_optimize(cal)
-  mp_initial(cal)
-  mp_initial(spec_with_R0)
+  
+  expect_equal(mp_initial_list(spec_with_R0)$R0, mp_initial_list(cal)$R0)
 
 })
 
 test_that("there is control over whether unused defaults are returned", {
-  spec = ("starter_models"
-    |> mp_tmb_library("sir", package = "macpan2")
-    |> mp_tmb_update(default = list(unused = 0)
-    )
-  )
-  expect_false("unused" %in% names(mp_default_list(spec)))
-  expect_true("unused" %in% names(mp_default_list(spec, include_all = TRUE)))
+  spec_with_unused = mp_tmb_update(spec, default = list(unused = 0))
+
+  expect_false("unused" %in% names(mp_default_list(spec_with_unused)))
+  expect_true("unused" %in% names(mp_default_list(spec_with_unused, include_all = TRUE)))
   
-  sim = mp_simulator(spec, 0L, "infection")
+  sim = mp_simulator(spec_with_unused, 0L, "infection")
   expect_false("unused" %in% names(mp_default_list(sim)))
   expect_true("unused" %in% names(mp_default_list(sim, include_all = TRUE)))
   
   data = mp_trajectory(sim)
-  cal = mp_tmb_calibrator(spec, data)
+  cal = mp_tmb_calibrator(spec_with_unused, data)
   
   expect_false("unused" %in% names(mp_default_list(cal)))
   expect_true("unused" %in% names(mp_default_list(cal, include_all = TRUE)))
