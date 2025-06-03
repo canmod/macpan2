bb9991a0543b44cf1aacba6d6a80527624cdfd15
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-06-03 11:44:40 -0400

    stochastic importation test

--- tests/testthat/test-absolute.R
+++ tests/testthat/test-absolute.R
@@ -25,5 +25,28 @@ test_that("absolute inflow definition and ode trajectory", {
       , method = "rk4"
     )
     
-    all.equal(reference_solution[-1, "N"], solution[, "value"])
+    expect_equal(reference_solution[-1, "N"], solution[, "value"])
+})
+
+
+test_that("stochastic absolute importation is plausible", {
+  s2 = mp_tmb_model_spec(
+      during = list(
+          N ~ S + I
+        , mp_per_capita_flow("S", "I", "beta * I / N", "infection")
+        , mp_inflow("I", "mu", "importation")
+      )
+    , default = list(S = 100, I = 0, beta = 0.25, mu = 0.02)
+  )
+  set.seed(7)
+  traj = (s2 
+    |> mp_discrete_stoch() 
+    |> mp_simulator(10, "I") 
+    |> mp_trajectory_par()
+  )
+  answer = structure(list(matrix = c("I", "I", "I", "I", "I", "I", "I", 
+  "I", "I", "I"), time = 1:10, row = c(0, 0, 0, 0, 0, 0, 0, 0, 
+  0, 0), col = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), value = c(1, 1, 
+  1, 1, 1, 1, 1, 1, 1, 3)), row.names = c(NA, -10L), class = "data.frame")
+  expect_equal(traj, answer)
 })
